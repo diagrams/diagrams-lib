e3329775c5bb9205ced380235fc92c319d89dc9c
walck <walck@lvc.edu>
2010-05-22 03:00:46 +0000

    Add rotations to TwoD

--- src/Diagrams/TwoD.hs
+++ src/Diagrams/TwoD.hs
@@ -5,6 +5,9 @@ import "diagrams-core" Graphics.Rendering.Diagrams
 import Graphics.Rendering.Diagrams.Expressions
 import Graphics.Rendering.Diagrams.Transform
 
+import Data.VectorSpace
+import Data.LinearMap
+
 import qualified Data.Map as M
 
 type P2 = (Double, Double)
@@ -29,5 +32,36 @@ box = Diagram [Prim (Box (-1,-1) (1,-1) (1,1) (-1,1))]
                           , (nm "LR", ( 1,-1))
                           , (nm "UR", ( 1, 1))
                           , (nm "UL", (-1, 1)) ])
-  where boxBounds (x,y) = let d = x*x + y*y
+  where boxBounds (x,y) = let d = x*x + y*y  -- want u.v/u.u, where u=(x,y), v=(1,1),(1,-1),(-1,1),(-1,-1)
                           in  maximum [(-x-y)/d, (x-y)/d, (x+y)/d, (y-x)/d]
+
+-- circle
+
+type Radius = Double
+
+data Circle = Circle P2 Radius
+
+{-
+need to handle ellipses
+
+circle :: (BSpace b ~ P2, Renderable Circle b) => Diagram b
+circle = Diagram [Prim (Circle (0,0) 1)]
+                 circleBounds
+                 (M.fromList [ (nm "C", ( 0, 0))
+                             , (nm "E", ( 1, 0))
+                             , (nm "N", ( 0, 1))
+                             , (nm "W", (-1, 0))
+                             , (nm "S", ( 0,-1)) ])
+  where circleBounds (x,y) = 1 / sqrt(x*x + y*y)
+-}
+
+-- rotation
+
+
+
+rotation theta = Affine (linear rot) zeroV where
+    rot (x,y) = (costh * x - sinth * y,sinth * x + costh * y)
+    costh = cos theta
+    sinth = sin theta
+
+rotate theta = transform $ rotation theta
