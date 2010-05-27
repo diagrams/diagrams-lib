eefb92480b858eddc293925dae9f2667fad08f16
Brent Yorgey <byorgey@cis.upenn.edu>
2010-05-27 18:06:55 +0000

    remove RelPaths and rework Path module a bit

--- src/Diagrams/Path.hs
+++ src/Diagrams/Path.hs
@@ -15,17 +15,14 @@
 
 module Diagrams.Path
        (
-         -- * Relative paths
+         -- * Paths
 
-         RelPath(..)
+         Path(..)
 
-       , relPathOffset
+         -- * Computing with paths
 
-         -- * Based paths
-
-       , Path(..)
-
-       , path
+       , pathOffset
+       , pathBounds
 
        ) where
 
@@ -42,55 +39,64 @@ import qualified Data.Map as M
 (<>) = mappend
 
 ------------------------------------------------------------
---  Relative paths  ----------------------------------------
-------------------------------------------------------------
-
--- | A /relative path/ is a sequence of segments placed end-to-end.
---   Like its constituent segments, a relative path is also
---   translationally invariant.
-newtype RelPath v = RelPath { relPathSegments :: [Segment v]
-                              -- ^ Get the list of segments comprising a relative path.
-                            }
-  deriving (Show, Functor)
-
--- | Relative paths form a monoid under path concatenation.
-instance Monoid (RelPath v) where
-  mempty = RelPath []
-  (RelPath b1) `mappend` (RelPath b2) = RelPath (b1 ++ b2)
-
-instance (AdditiveGroup v, Transformable v) => Transformable (RelPath v) where
-  type TSpace (RelPath v)  = TSpace v
-  transform = fmap . transform
-
--- | Compute the total offset from the start of a relative path to the
---   end.
-relPathOffset :: (AdditiveGroup v) => RelPath v -> v
-relPathOffset = sumV . map segOffset . relPathSegments
-
-------------------------------------------------------------
 --  Paths  -------------------------------------------------
 ------------------------------------------------------------
 
--- | A /path/ is a relative path together with a fixed starting point;
---   hence, paths are /not/ translationally invariant.
+-- | A /path/ is a sequence of segments placed end-to-end, together
+--   with a distinguished starting point.  Hence, paths are /not/
+--   translationally invariant.  Paths can also be /open/ or /closed/.
 data Path v = Path { isClosed     :: Bool
                    , pathStart    :: v
-                   , pathSegments :: (RelPath v)
+                   , pathSegments :: [Segment v]
                    }
   deriving (Show, Functor)
 
+-- | Paths form a monoid under path concatenation.  @p1 `mappend` p2@
+--   is the path formed by taking the starting point of @p1@ and
+--   appending the list of @p2@'s segments onto the end of @p1@'s list
+--   (discarding the starting point of @p2@) --- unless @p1@ is the
+--   empty path with no segments, in which case @p1 `mappend` p2@ is
+--   just @p2@.  @p1 `mappend` p2@ is closed iff either @p1@ or @p2@
+--   are.
+instance (AdditiveGroup v) => Monoid (Path v) where
+  mempty = Path False zeroV []
+  Path c1 _ []     `mappend` Path c2 st segs = Path (c1 || c2) st segs
+  Path c1 st segs1 `mappend` Path c2 _ segs2 = Path (c1 || c2) st (segs1 ++ segs2)
+
 instance (AdditiveGroup v, Transformable v) => Transformable (Path v) where
-  type TSpace (Path v)   = TSpace v
+  type TSpace (Path v) = TSpace v
   transform = fmap . transform
 
--- Build a zero-based path from a list of segments.
+------------------------------------------------------------
+--  Computing with paths  ----------------------------------
+------------------------------------------------------------
 
+-- | Compute the total offset from the start of a path to the end.
+pathOffset :: (AdditiveGroup v) => Path v -> v
+pathOffset = sumV . map segOffset . pathSegments
+
+{- XXX TODO:
+     - total arc length of a path
+     - path splitting methods?
+-}
+
+-- | Compute the bounding function for an entire path.
+pathBounds :: (InnerSpace v, AdditiveGroup (Scalar v), Floating (Scalar v), Ord (Scalar v))
+           => Path v -> Bounds v
+pathBounds (Path _ st segs) = rebaseBounds (negateV st) $
+  foldr (\seg bds -> rebaseBounds (negateV (segOffset seg)) bds
+                     <> segmentBounds seg)
+        mempty
+        segs
+
+-- Build a zero-based path from a list of segments.
+{-
 path :: ( InnerSpace v, Floating (Scalar v), AdditiveGroup (Scalar v), Ord (Scalar v)
         , TSpace v ~ v)
      => (Renderable (Path v) b, BSpace b ~ v) => [v] -> Diagram b
 path ss = Diagram [Prim (Path False zeroV (RelPath segs))]
                   pathBounds
                   mempty
-  where pathBounds = foldr (\seg bds -> rebaseBounds (negateV (segOffset seg)) bds
-                                        <> segmentBounds seg) mempty segs
+  where pathBounds =
         segs = map Linear ss
+-}
