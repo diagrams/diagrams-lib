ab902ab2854144de536c09f9aae246c2ff9c7830
Brent Yorgey <byorgey@cis.upenn.edu>
2010-11-30 16:44:11 +0000

    fix Ellipse.hs.  Ellipses are now represented by an affine transformation of the unit circle.

--- src/Diagrams/TwoD/Ellipse.hs
+++ src/Diagrams/TwoD/Ellipse.hs
@@ -16,98 +16,32 @@ module Diagrams.TwoD.Ellipse
     ( Ellipse(..)
     , circle
     , ellipse
+    , ellipseCoeffs
     , ellipseCenter
     , ellipseAngle
     , ellipseScale
-    , ellipseCenterScaleAngle
     ) where
 
 import "diagrams-core" Graphics.Rendering.Diagrams
 import Graphics.Rendering.Diagrams.Transform
+import Graphics.Rendering.Diagrams.Util
 
 import Diagrams.TwoD.Types
 import Diagrams.TwoD.Transform
 
-import Data.VectorSpace
-import Data.LinearMap
+import Data.Monoid (Any(..), mempty)
 
-import Data.Monoid (Any(..))
+-- | An ellipse is represented by an affine transformation acting on
+--   the unit circle.
+data Ellipse = Ellipse (Transformation R2)
 
-type R3 = (Double, Double, Double)
-type Aug3 = (R2,Double)
-
-type FlatFunc = R3 -> R3
-type AugFunc = Aug3 -> Aug3
-
-data Ellipse = Ellipse Double Double Double Double Double Double
-               deriving (Show)
--- 6 Doubles are A, B, C, D, E, F in A x^2 + B x y + C y^2 + D x + E y + F = 0
-
--- Perhaps this can be done more simply, now that we have the transpose available?
 instance Transformable Ellipse where
   type TSpace Ellipse = R2
-  transform = undefined
-  {-
-  transform (Transformation t _ _) ell
-      = ellipseFromFunc ((aug3Transpose tinv)
-                         . (funcFromEllipse ell)
-                         . tinv) where
-                               tinv = lapp $ linv t
-  -}
+  transform t (Ellipse e) = Ellipse (t <> e)
 
-func3Transpose :: (R3 -> R3) -> (R3 -> R3)
-func3Transpose func3 = \v -> (v1 `dot` v
-                             ,v2 `dot` v
-                             ,v3 `dot` v) where
-    (v1,v2,v3) = columnsFromLinearMap func3
-
-aug3Transpose :: (Aug3 -> Aug3) -> (Aug3 -> Aug3)
-aug3Transpose = augmentFunc . func3Transpose . flattenFunc
-
-linearMapFromColumns (v1,v2,v3) = linear $ \(x,y,z) -> (x*^v1,y*^v2,z*^v3)
-
-columnsFromLinearMap lmap = (v1,v2,v3) where
-    v1 = lmap (1,0,0)
-    v2 = lmap (0,1,0)
-    v3 = lmap (0,0,1)
-
-aug :: R3 -> Aug3
-aug (a,b,c) = ((a,b),c)
-
-unAug :: Aug3 -> R3
-unAug ((a,b),c) = (a,b,c)
-
-augmentFunc :: (R3 -> R3) -> (Aug3 -> Aug3)
-augmentFunc f = aug . f . unAug
-
-flattenFunc :: (Aug3 -> Aug3) -> (R3 -> R3)
-flattenFunc g = unAug . g . aug
-
-cross :: (Double,Double,Double) -> (Double,Double,Double) -> (Double,Double,Double)
-cross (ax,ay,az) (bx,by,bz) = (ay * bz - az * by,az * bx - ax * bz,ax * by - ay * bx)
-
-dot :: (Double,Double,Double) -> (Double,Double,Double) -> Double
-dot (ax,ay,az) (bx,by,bz) = ax * bx + ay * by + az * bz
-
-funcFromEllipse :: Ellipse -> AugFunc
-funcFromEllipse (Ellipse a b c d e f)
-    = \((x,y),z) -> ((a   * x + b/2 * y + d/2 * z
-                     ,b/2 * x + c   * y + e/2 * z)
-                    ,d/2 * x + e/2 * y + f   * z)
-
-ellipseFromFunc :: AugFunc -> Ellipse
-ellipseFromFunc func
-    = Ellipse a b c d e f where
-      ((a     ,halfb1),halfd1) = func ((1,0),0)
-      ((halfb2,c     ),halfe1) = func ((0,1),0)
-      ((halfd2,halfe2),f     ) = func ((0,0),1)
-      b = halfb1 + halfb2
-      d = halfd1 + halfd2
-      e = halfe1 + halfe2
-
--- | A unit circle
+-- | A unit circle.
 circle :: (BSpace b ~ R2, Renderable Ellipse b) => Diagram b
-circle = Diagram (prim $ Ellipse 1 0 1 0 0 (-1))
+circle = Diagram (prim $ Ellipse mempty)
                  (Bounds circleBounds)
                  (fromNames [ ("C", P ( 0, 0))
                             , ("E", P ( 1, 0))
@@ -117,37 +51,59 @@ circle = Diagram (prim $ Ellipse 1 0 1 0 0 (-1))
                  (\(P (x,y)) -> Any (x*x + y*y <= 1))
     where circleBounds (x,y) = 1 / sqrt(x*x + y*y)
 
--- | Ellipse with eccentricity e
+-- | Construct an ellipse with eccentricity e, created by scaling the
+--   unit circle in the Y direction.  The eccentricity must be within
+--   the interval [0,1).
 ellipse :: (BSpace b ~ R2, Renderable Ellipse b) => Double -> Diagram b
 ellipse e
     | e >= 0 && e < 1  = scaleY (sqrt (1 - e^2)) circle
     | otherwise        = error "Eccentricity of ellipse must be >= 0 and < 1."
 
--- | Returns (xCenter,yCenter)
+-- | Compute the coefficients of the quadratic form
+--
+--     A x^2 + B x y + C y^2 + D x + E y + F = 0
+--
+--   for an ellipse.  Returns A through F (in that order) in a tuple.
+ellipseCoeffs :: Ellipse -> (Double, Double, Double, Double, Double, Double)
+ellipseCoeffs (Ellipse eT) = (      a*a + d*d      -- x^2
+                             , 2 * (a*b + d*e)     -- xy
+                             ,      b*b + e*e      -- y^2
+                             , 2 * (a*c + d*f)     -- x
+                             , 2 * (b*c + e*f)     -- y
+                             ,      c*c + f*f - 1
+                             )
+  where eT'   = inv eT
+        (a,d) = apply eT' (1,0)
+        (b,e) = apply eT' (0,1)
+        (c,f) = transl eT'
+
+
+-- | Compute the center of an ellipse.
 ellipseCenter :: Ellipse -> P2
-ellipseCenter (Ellipse a b c d e f)
-    = P $ (2*c*d-b*e,2*a*e-b*d) ^/ (b*b - 4*a*c)
+ellipseCenter (Ellipse e) = papply e origin
 
--- | Returns long axis angle, measured counterclockwise from x axis toward y axis
+-- Below formulas taken from http://mathworld.wolfram.com/Ellipse.html
+
+-- | Compute the angle to the major axis, measured
+--   counterclockwise from the positive x axis.
 ellipseAngle :: Ellipse -> Angle
-ellipseAngle (Ellipse a b c d e f)
-    = atan2 (sqrt((c - a)^2 + b^2) + c - a) b
+ellipseAngle ell
+  | b == 0 && a <= c  = 0
+  | b == 0 && a >  c  = pi/2
+  | a <= c            = atan (b/(a-c)) / 2
+  | otherwise         = (pi + atan (b/(a-c))) / 2
+  where (a,b,c,_,_,_) = ellipseCoeffs ell
 
--- | Returns (xScale,yScale)
-ellipseScale :: Ellipse -> R2
-ellipseScale (Ellipse a b c d e f)
-    = (1/sqrt(lam1),1/sqrt(lam2)) where
-      lam1 = (a + c + sqrt((a - c)^2 + b^2))/(-2 * f)
-      lam2 = (a + c - sqrt((a - c)^2 + b^2))/(-2 * f)
-
--- | Returns (xCenter,yCenter,xScale,yScale,angle)
-ellipseCenterScaleAngle :: Ellipse -> (Double,Double,Double,Double,Double)
-ellipseCenterScaleAngle (Ellipse a b c d e f)
-    = (xc,yc,xs,ys,th) where  -- xc = x center, ys = y scale, th = angle
-      (xc,yc) = (2*c*d-b*e,2*a*e-b*d) ^/ (b*b - 4*a*c)
-      th = atan2 (sqrt((c - a)^2 + b^2) + c - a) b
-      (xs,ys) = (1/sqrt(lam1),1/sqrt(lam2))
-      lam1 = (a + c + sqrt((a - c)^2 + b^2))/(-2 * f')
-      lam2 = (a + c - sqrt((a - c)^2 + b^2))/(-2 * f')
-      f' = f - (a * xc^2 + b * xc * yc + c * yc^2)
-
+-- | Compute the scaling factors of the ellipse, i.e. (a,b) where a and b are
+--   half the lengths of the major and minor axes respectively.
+ellipseScale :: Ellipse -> (Double, Double)
+ellipseScale ell = ( sqrt (num / (r * ( disc - (a+c))))
+                   , sqrt (num / (r * (-disc - (a+c))))
+                   )
+  where num  = 2*(a*f*f + c*d*d + g*b*b - 2*b*d*f - a*c*g)
+        disc = sqrt ((a - c)^2 +4*b*b)
+        r    = (b*b - a*c)
+        (a,b',c,d',f',g) = ellipseCoeffs ell
+        b = b'/2
+        d = d'/2
+        f = f'/2
