851ee510529834c337773b634700aef95316bcf9
ksuvee <ksuvee@seas.upenn.edu>
2010-12-19 00:32:58 +0000

    final_project

--- src/Diagrams/TwoD/Shapes.hs
+++ src/Diagrams/TwoD/Shapes.hs
@@ -15,11 +15,16 @@
 module Diagrams.TwoD.Shapes
        ( Box(..)
        , box
+       , polygonDiagrams
        ) where
 
 import Graphics.Rendering.Diagrams
 
 import Diagrams.TwoD.Types
+
+import Diagrams.Path
+
+import Diagrams.TwoD.Transform
 
 import qualified Data.Map as M
 
@@ -46,4 +51,80 @@ box = Diagram (prim $ Box (P (-1,-1)) (P (1,-1)) (P (1,1)) (P (-1,1)))
   where boxBounds (x,y) = let d = x*x + y*y  -- want u.v/u.u, where u=(x,y), v=(1,1),(1,-1),(-1,1),(-1,-1)
                               in  maximum [(-x-y)/d, (x-y)/d, (x+y)/d, (y-x)/d]
         inBox x = x >= (-1) && x <= 1
+
+-- | Take number of sides of polygon then create diagrams with these numbers of equal length  sides
+polygonDiagrams:: (BSpace b ~ R2, Renderable (Path R2) b) => Int -> Diagram b
+polygonDiagrams n = stroke (polygonPath (polygonVertices n))
+
+-- | Build a path from the given list of vertices
+polygonPath:: [P2] -> Path R2
+polygonPath xs = close $ pathFromVertices xs
+
+-- | Construct list of vertices from the given sides 
+polygonVertices:: Int -> [P2]
+polygonVertices n = map (aux n) [1..(n)]
+
+-- | helper function
+--   first arguement = numbers of side
+--   second arguement = times
+aux:: Int -> Int -> P2
+aux n t = rotate angle starting
+   where starting = translateX 1 origin
+         angle = (2*pi*fromIntegral t) / fromIntegral n
+
+{--
+-- | Equilateral triangle path Builder
+--   a function to construct the path for drawing equilateral triangle
+eqTrianglePath :: P2 -> Path R2
+eqTrianglePath p = pathFromOffsets p (initPointTri p)
+
+-- | Equilateral triangle drawing function
+eqtriangleDiagram :: (BSpace b ~ R2, Renderable (Path R2) b) =>  P2 -> Diagram b
+eqTriangleDiagram = stroke $ eqTrianglePath
+
+-- | Equilateral offset genrator
+initPointEqTri :: P2 -> [R2]
+initPointEqTri (P r) = r1 : r2 : []
+    where r1 = translateY (snd r) (translateX (fst r) (rotate (pi/3) r))
+          r2 = translateY (snd r1) (translateX (fst r1) (rotate (pi/3) r1))
+
+-- | BuildTriangleDiagram, create a triangle of specific angle
+buildingTri :: Angle -> Angle -> P2 -> Diagram R2
+buildingTri = undefined
+
+-- | BuildTrianglePath, create a path for drawing a diagram
+buildingTriPath :: Angle -> Angle
+
+
+-- | Box Diagram
+createBox:: (BSpace b ~ R2, Renderable (Path R2) b) =>  P2 -> Diagram b 
+createBox p = stroke.close.pathFromVertices $ [p, translateX 1 p, translateX 1 . translateY 1 p, translateY 1 p]
+--}
+
+{--NOT WORK
+-- | Polygon drawing function, draw a polygon of maximum side of 6
+--   create a polygon of side (n) up to 6. The default length of each side is 1
+polygonDrawing :: Int -> P2 -> Diagram R2
+polygonDrawing
+
+-- | Polygon path building function, create a path for the polygonDrawing
+--   create a path for drawing different types of polygon beginning at the given starting point
+--   Nothing if side > 6 or side < 1; otherwise Maybe
+buildPolyPath :: Int -> P2 -> Maybe (Path R2)
+buildPolyPath s p 
+	| s == 1 = undefined
+	| s == 2 = undefined 
+	| s == 3 = undefined
+	| s == 4 = undefined 
+	| s == 5 = undefined
+	| s == 6 = undefined 
+	| otherwise = Nothing
+
+-- | BuildDiagram, create a diagram from the given path
+--   if Nothing then draw none, otherwise create a diagram
+buildDiagram :: Maybe (Path R2) -> Diagram R2
+buildDiagram Nothing   = undefined
+buildDiagram Just path = stroke path--}
+
+
 
