c2cd277f2b6544b3dbdb5b517f1d857a57b0abb9
ksuvee <ksuvee@seas.upenn.edu>
2010-12-20 02:00:06 +0000

    Sources_code

--- src/Diagrams/TwoD/Shapes.hs
+++ src/Diagrams/TwoD/Shapes.hs
@@ -15,7 +15,9 @@
 module Diagrams.TwoD.Shapes
        ( Box(..)
        , box
-       , polygonDiagrams
+       , polygon
+       , square
+       , createStar
        ) where
 
 import Graphics.Rendering.Diagrams
@@ -53,22 +55,39 @@ box = Diagram (prim $ Box (P (-1,-1)) (P (1,-1)) (P (1,1)) (P (-1,1)))
         inBox x = x >= (-1) && x <= 1
 
 -- | Take number of sides of polygon then create diagrams with these numbers of equal length  sides
-polygonDiagrams:: (BSpace b ~ R2, Renderable (Path R2) b) => Int -> Diagram b
-polygonDiagrams n = stroke (polygonPath (polygonVertices n))
+--   the generated polygon will be fit into the unit circle which has radius of 1. 
+--   each point of the polygon will be located on the circumference of the unit circle
+polygon:: (BSpace b ~ R2, Renderable (Path R2) b) => Int -> Diagram b
+polygon n = stroke . polygonPath $ n
 
--- | Build a path from the given list of vertices
-polygonPath:: [P2] -> Path R2
-polygonPath xs = close $ pathFromVertices xs
+-- | Take numbers of sides as an input and generate the path to be rendered
+--   a helper function for polygon
+polygonPath:: Int -> Path R2
+polygonPath n = close $ pathFromVertices (polygonVertices n)
 
 -- | Construct list of vertices from the given sides 
+--   a helper function for polygonPath
 polygonVertices:: Int -> [P2]
-polygonVertices n = map (aux n) [1..(n)]
+polygonVertices n = map (vertexGen n) [1..(n)]
+	where vertexGen:: Int -> Int -> P2
+	      vertexGen n t  = let starting = translateX 1 origin
+              	     		   angle    = (2*pi*fromIntegral t) / fromIntegral n
+		    		   in rotate angle starting
+   	      	 
+-- | Generate a sqaure which has its center at the origin(0,0) 
+--   each side of the square will have length of one
+square ::  (BSpace b ~ R2, Renderable (Path R2) b) => Diagram b
+square = stroke . close $ pathFromVertices $ [origin, translateX 1 origin, translateX 1 (translateY 1 origin), translateY 1 origin]
 
--- | helper function
---   first arguement = numbers of side
---   second arguement = times
-aux:: Int -> Int -> P2
-aux n t = rotate angle starting
-   where starting = translateX 1 origin
-         angle = (2*pi*fromIntegral t) / fromIntegral n
 
+-- | Generate a star polygon 
+--   take numbers of angle as one of its arguement
+--   Formula: (pi*(p - 2q))/p ; p is the first arguement and q is the second arguement
+createStar :: (BSpace b ~ R2, Renderable (Path R2) b) => Int -> Int-> Diagram b
+createStar p q =  stroke . close $ pathFromVertices $ createStarH origin p q p 0
+
+createStarH :: P2 -> Int -> Int -> Int -> Angle -> [P2]
+createStarH _ _ _ 0 _ = []
+createStarH v@(P r) p q n a = (point : createStarH point p q (n - 1) angle)
+          where angle = (a + pi - (pi*(fromIntegral p-2*(fromIntegral q))/fromIntegral p))
+                point = translateY (snd r) . translateX (fst r) $ rotate angle (translateY 1 origin)
