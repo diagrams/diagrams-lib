6f01c713da4e6d335755a5c125e7a324640fe4ca
Brent Yorgey <byorgey@cis.upenn.edu>
2010-12-24 21:53:01 +0000

    clean up polygon functions and export them

--- src/Diagrams/TwoD/Shapes.hs
+++ src/Diagrams/TwoD/Shapes.hs
@@ -15,17 +15,15 @@
 module Diagrams.TwoD.Shapes
        ( Box(..)
        , box
-       , polygon
+       , polygon, polygonPath, polygonVertices
        , square
        , createStar
        ) where
 
 import Graphics.Rendering.Diagrams
 
-import Diagrams.TwoD.Types
-
 import Diagrams.Path
-
+import Diagrams.TwoD.Types
 import Diagrams.TwoD.Transform
 
 import qualified Data.Map as M
@@ -54,38 +52,35 @@ box = Diagram (prim $ Box (P (-1,-1)) (P (1,-1)) (P (1,1)) (P (-1,1)))
                               in  maximum [(-x-y)/d, (x-y)/d, (x+y)/d, (y-x)/d]
         inBox x = x >= (-1) && x <= 1
 
--- | Take number of sides of polygon then create diagrams with these numbers of equal length  sides
---   the generated polygon will be fit into the unit circle which has radius of 1. 
---   each point of the polygon will be located on the circumference of the unit circle. The center of the polygon is located at position (0,0)
+-- | Create a regular polygon with the given number of sides, with a
+--   radius (distance from the center to any vertex) of one, and a
+--   vertex at (1,0).
 polygon:: (BSpace b ~ R2, Renderable (Path R2) b) => Int -> Diagram b
-polygon n = stroke . polygonPath $ n
+polygon = stroke . polygonPath
 
--- | Take numbers of sides as an input and generate the path to be rendered
---   a helper function for polygon
+-- | Create a closed, radius-one regular polygonal path with the given
+--   number of edges, with a vertex at (1,0).
 polygonPath:: Int -> Path R2
-polygonPath n = close $ pathFromVertices (polygonVertices n)
+polygonPath = close . pathFromVertices . polygonVertices
 
--- | Construct list of vertices from the given sides 
---   a helper function for polygonPath
+-- | Generate the vertices of a radius-one regular polygon with the
+--   given number of sides, with a vertex at (1,0).
 polygonVertices:: Int -> [P2]
-polygonVertices n = map (vertexGen n) [1..(n)]
-	where vertexGen:: Int -> Int -> P2
-	      vertexGen n t  = let starting = translateX 1 origin
-              	     		   angle    = (2*pi*fromIntegral t) / fromIntegral n
-		    		   in rotate angle starting
-   	      	 
--- | Generate a sqaure which has its center at the origin(0,0) 
+polygonVertices n = take n . iterate (rotate angle) $ start
+  where start = translateX 1 origin
+        angle = 2*pi / fromIntegral n
+
+-- | Generate a sqaure which has its center at the origin(0,0)
 --   each side of the square will have length of one unit
 square ::  (BSpace b ~ R2, Renderable (Path R2) b) => Diagram b
 square = stroke . close $ pathFromVertices $ [origin, translateX 1 origin, translateX 1 (translateY 1 origin), translateY 1 origin]
 
-
--- | Generate a star polygon 
+-- | Generate a star polygon
 --   Formula: (pi*(p - 2q))/p ; p is the first arguement and q is the second arguement
 createStar :: (BSpace b ~ R2, Renderable (Path R2) b) => Int -> Int-> Diagram b
 createStar p q =  stroke . close $ pathFromVertices $ createStarH origin p q p 0
 
--- | Create a path to draw a star 
+-- | Create a path to draw a star
 --   take staring point(P2) as an arguement; p and q from the star formula;cumulative angle as the third argument; star tracker as the forth arguement
 createStarH :: P2 -> Int -> Int -> Int -> Angle -> [P2]
 createStarH _ _ _ 0 _ = []
