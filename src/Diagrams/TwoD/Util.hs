39deef1cba4c9420e39a6a74e197f09f85996bd7
Brent Yorgey <byorgey@cis.upenn.edu>
2011-04-26 23:44:55 +0000

    remove unneeded constraints on functions in TwoD.Util

--- src/Diagrams/TwoD/Util.hs
+++ src/Diagrams/TwoD/Util.hs
@@ -25,32 +25,32 @@ import Diagrams.TwoD.Types
 import Control.Arrow ((***), (&&&))
 
 -- | Compute the width of a diagram.
-width :: Backend b R2 => AnnDiagram b R2 m -> Double
+width :: AnnDiagram b R2 m -> Double
 width = negate . uncurry (-) . extentX
 
 -- | Compute the height of a diagram.
-height :: Backend b R2 => AnnDiagram b R2 m -> Double
+height :: AnnDiagram b R2 m -> Double
 height = negate . uncurry (-) . extentY
 
 -- | Compute the width and height of a diagram.
-size2D :: Backend b R2 => AnnDiagram b R2 m -> (Double, Double)
+size2D :: AnnDiagram b R2 m -> (Double, Double)
 size2D = width &&& height
 
 -- | Compute the absolute x-coordinate range of a diagram in R2, in
 --   the form (lo,hi).
-extentX :: Backend b R2 => AnnDiagram b R2 a -> (Double, Double)
+extentX :: AnnDiagram b R2 a -> (Double, Double)
 extentX d = (-f (-1,0), f (1,0))
   where f = appBounds $ getBounds d
 
 -- | Compute the absolute y-coordinate range of a diagram in R2, in
 --   the form (lo,hi).
-extentY :: Backend b R2 => AnnDiagram b R2 a -> (Double, Double)
+extentY :: AnnDiagram b R2 a -> (Double, Double)
 extentY d = (-f (0,-1), f (0,1))
   where f = appBounds $ getBounds d
 
 -- | Compute the point at the center (in the x- and y-directions) of a
 --   diagram.
-center2D :: Backend b R2 => AnnDiagram b R2 a -> P2
+center2D :: AnnDiagram b R2 a -> P2
 center2D = P . (mid *** mid) . (extentX &&& extentY)
   where mid = (/2) . uncurry (+)
 
