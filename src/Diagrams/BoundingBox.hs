7d9f32f4712294f54428e072d01cdf9c4cfc30da
Brent Yorgey <byorgey@cis.upenn.edu>
2011-05-31 20:13:02 +0000

    clean up and include BoundingBox module

--- src/Diagrams/BoundingBox.hs
+++ src/Diagrams/BoundingBox.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DeriveDataTypeable
+           , DeriveFunctor
            , FlexibleContexts
            , NoMonomorphismRestriction
            , TypeFamilies
+           , ScopedTypeVariables
   #-}
 -----------------------------------------------------------------------------
 -- |
@@ -15,80 +17,155 @@
 -----------------------------------------------------------------------------
 
 module Diagrams.BoundingBox
-       ( BoundingBox(), boundingBox, fromPoint, fromPoints
+       ( -- * Bounding boxes
+         BoundingBox()
+
+         -- * Constructing bounding boxes
+       , fromCorners, fromPoint, fromPoints
+       , boundingBox
+
+         -- * Operations on bounding boxes
        , contains, contains'
        , inside, inside', outside, outside'
        , union, intersection, unions, intersections
        ) where
 
+import Control.Applicative ((<*>))
 import Control.Monad (join, liftM2)
 import Data.Map (Map, fromList, toList, fromDistinctAscList, toAscList)
 import qualified Data.Foldable as F
 
-import Data.VectorSpace (VectorSpace, Scalar, AdditiveGroup, zeroV)
-import Data.Basis (HasBasis, Basis, decompose, recompose)
-import Data.AffineSpace ((.-.))
+import Data.Maybe (fromJust)
+
+import Data.VectorSpace (VectorSpace, Scalar, AdditiveGroup, zeroV, negateV)
+import Data.Basis (HasBasis, Basis, decompose, recompose, basisValue)
 
 import Data.Data (Data)
 import Data.Typeable (Typeable)
 
-import Graphics.Rendering.Diagrams.Points (Point, origin)
+import Graphics.Rendering.Diagrams.Points (Point(..))
+import Graphics.Rendering.Diagrams.Transform (Transformable(..), HasLinearMap)
+import Graphics.Rendering.Diagrams.HasOrigin (HasOrigin(..))
+import Graphics.Rendering.Diagrams.Bounds (Boundable, boundary)
 import Graphics.Rendering.Diagrams.V (V)
 
-data BoundingBox v = BoundingBox v v
-  deriving (Show, Read, Eq, Data, Typeable)
+-- | A bounding box is an axis-aligned region determined
+--   by two points indicating its \"lower\" and \"upper\" corners.
+data BoundingBox v = BoundingBox (Point v) (Point v)
+  deriving (Show, Read, Eq, Data, Typeable, Functor)
 
 type instance V (BoundingBox v) = v
 
-boundingBox
-  :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
-  => v -> v -> BoundingBox v
-boundingBox u v = BoundingBox (toVector (combineV min u v)) (toVector (combineV max u v))
+instance (HasLinearMap v, Transformable v) => Transformable (BoundingBox v) where
+  transform t (BoundingBox p1 p2) = BoundingBox (transform t p1)
+                                                (transform t p2)
 
+instance VectorSpace v => HasOrigin (BoundingBox v) where
+  moveOriginTo p (BoundingBox p1 p2) = BoundingBox (moveOriginTo p p1)
+                                                   (moveOriginTo p p2)
+
+-- | Create a bounding box from any two opposite corners.
+fromCorners
+  :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
+  => Point v -> Point v -> BoundingBox v
+fromCorners u v = BoundingBox (toPoint (combineV min u v))
+                              (toPoint (combineV max u v))
+
+-- | Create a degenerate bounding \"box\" containing only a single point.
 fromPoint
   :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
   => Point v -> BoundingBox v
-fromPoint p = let v = p .-. origin in BoundingBox v v
+fromPoint p = BoundingBox p p
 
+-- | Create the smallest bounding box containing all the given points.
 fromPoints
   :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
   => [Point v] -> Maybe (BoundingBox v)
-fromPoints [] = Nothing
-fromPoints ps = Just . foldr1 union . map fromPoint $ ps
+fromPoints = unions . map fromPoint
 
-contains, contains'
+-- | Create a bounding box for any boundable object (such as a diagram or path).
+boundingBox :: forall a. (Boundable a, HasBasis (V a), Ord (Basis (V a)))
+            => a -> BoundingBox (V a)
+boundingBox a = fromJust . fromPoints . map (flip boundary a) $ [id, negateV] <*> units
+  where units = map (basisValue . fst) (decompose (zeroV :: V a))
+
+-- | Check whether a point is contained in a bounding box (including its edges).
+contains
   :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
   => BoundingBox v -> Point v -> Bool
-contains  (BoundingBox l h) p = F.and (combineV (<=) l v) && F.and (combineV (<=) v h) where v = p .-. origin
-contains' (BoundingBox l h) p = F.and (combineV (< ) l v) && F.and (combineV (< ) v h) where v = p .-. origin
+contains (BoundingBox l h) p = F.and (combineV (<=) l p)
+                            && F.and (combineV (<=) p h)
 
-unions, intersections
+-- | Check whether a point is /strictly/ contained in a bounding box.
+contains'
+  :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
+  => BoundingBox v -> Point v -> Bool
+contains' (BoundingBox l h) p = F.and (combineV (< ) l p)
+                             && F.and (combineV (< ) p h)
+
+-- | Compute the smallest bounding box containing all the given
+--   bounding boxes (or @Nothing@ if the list is empty).
+unions
   :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
   => [BoundingBox v] -> Maybe (BoundingBox v)
 unions [] = Nothing
 unions ps = Just . foldr1 union $ ps
+
+-- | Compute the largest bounding box contained in all the given
+--   bounding boxes (or @Nothing@ is the list is empty).
+intersections
+  :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
+  => [BoundingBox v] -> Maybe (BoundingBox v)
 intersections [] = Nothing
 intersections ps = foldr1 ((join .) . liftM2 intersection) (map Just ps)
 
-inside, inside', outside, outside'
+-- | Test whether the first bounding box is contained inside
+--   the second.
+inside
   :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
   => BoundingBox v -> BoundingBox v -> Bool
-inside   (BoundingBox ul uh) (BoundingBox vl vh) =  F.and (combineV (<=) uh vh) && F.and (combineV (>=) ul vl)
-inside'  (BoundingBox ul uh) (BoundingBox vl vh) =  F.and (combineV (< ) uh vh) && F.and (combineV (> ) ul vl)
-outside  (BoundingBox ul uh) (BoundingBox vl vh) =  F.or  (combineV (<=) uh vl) || F.or  (combineV (>=) ul vh)
-outside' (BoundingBox ul uh) (BoundingBox vl vh) =  F.or  (combineV (< ) uh vl) || F.or  (combineV (> ) ul vh)
+inside   (BoundingBox ul uh) (BoundingBox vl vh) =  F.and (combineV (<=) uh vh)
+                                                 && F.and (combineV (>=) ul vl)
 
+-- | Test whether the first bounding box is /strictly/ contained
+--   inside the second.
+inside'
+  :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
+  => BoundingBox v -> BoundingBox v -> Bool
+inside'  (BoundingBox ul uh) (BoundingBox vl vh) =  F.and (combineV (< ) uh vh)
+                                                 && F.and (combineV (> ) ul vl)
+
+-- | Test whether the first bounding box lies outside the second
+--   (although they may intersect in their boundaries).
+outside
+  :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
+  => BoundingBox v -> BoundingBox v -> Bool
+outside  (BoundingBox ul uh) (BoundingBox vl vh) =  F.or  (combineV (<=) uh vl)
+                                                 || F.or  (combineV (>=) ul vh)
+
+-- | Test whether the first bounding box lies /strictly/ outside the second
+--   (they do not intersect at all).
+outside'
+  :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
+  => BoundingBox v -> BoundingBox v -> Bool
+outside' (BoundingBox ul uh) (BoundingBox vl vh) =  F.or  (combineV (< ) uh vl)
+                                                 || F.or  (combineV (> ) ul vh)
+
+-- | Form the largest bounding box contained within this given two
+--   bounding boxes, or @Nothing@ if the two bounding boxes do not
+--   overlap at all.
 intersection
   :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
   => BoundingBox v -> BoundingBox v -> Maybe (BoundingBox v)
 intersection u@(BoundingBox ul uh) v@(BoundingBox vl vh)
   | u `outside'` v = Nothing
-  | otherwise = Just (boundingBox (toVector (combineV max ul vl)) (toVector (combineV min uh vh)))
+  | otherwise = Just (fromCorners (toPoint (combineV max ul vl)) (toPoint (combineV min uh vh)))
 
+-- | Form the smallest bounding box containing the given two bounding boxes.
 union
   :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v), Ord (Scalar v))
   => BoundingBox v -> BoundingBox v -> BoundingBox v
-union (BoundingBox ul uh) (BoundingBox vl vh) = BoundingBox (toVector (combineV min ul vl)) (toVector (combineV max uh vh))
+union (BoundingBox ul uh) (BoundingBox vl vh) = BoundingBox (toPoint (combineV min ul vl)) (toPoint (combineV max uh vh))
 
 -- internals using Map (Basis v) (Scalar v)
 -- probably paranoia, but decompose might not always
@@ -98,18 +175,20 @@ union (BoundingBox ul uh) (BoundingBox vl vh) = BoundingBox (toVector (combineV 
 fromVector :: (HasBasis v, Ord (Basis v)) => v -> Map (Basis v) (Scalar v)
 fromVector = fromList . decompose
 
-toVector :: HasBasis v => Map (Basis v) (Scalar v) -> v
-toVector = recompose . toList
+toPoint :: HasBasis v => Map (Basis v) (Scalar v) -> Point v
+toPoint = P . recompose . toList
 
-combineV :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v)) => (Scalar v -> Scalar v -> a) -> v -> v -> Map (Basis v) a
-combineV f u v = combineDefault zeroV zeroV f (fromVector u) (fromVector v)
+combineV :: (HasBasis v, Ord (Basis v), AdditiveGroup (Scalar v)) => (Scalar v -> Scalar v -> a) -> Point v -> Point v -> Map (Basis v) a
+combineV f (P u) (P v) = combineDefault zeroV zeroV f (fromVector u) (fromVector v)
 
 combineDefault :: Ord k => a -> b -> (a -> b -> c) -> Map k a -> Map k b -> Map k c
 combineDefault a b f = combine g
   where
+    g Nothing  Nothing  = f a b
     g Nothing  (Just y) = f a y
     g (Just x) Nothing  = f x b
     g (Just x) (Just y) = f x y
+
 
 combine :: Ord k => (Maybe a -> Maybe b -> c) -> Map k a -> Map k b -> Map k c
 combine f am bm = fromDistinctAscList $ merge (toAscList am) (toAscList bm)
