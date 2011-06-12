5d273552d743e4f57855a1966d4e091c1853e0f7
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-12 20:17:23 +0000

    export new stroke' function that lets you assign names to path vertices; also document and export path inclusion tests

--- src/Diagrams/TwoD/Path.hs
+++ src/Diagrams/TwoD/Path.hs
@@ -19,7 +19,13 @@
 module Diagrams.TwoD.Path
        ( -- * Constructing path-based diagrams
 
-         stroke, strokeT
+         stroke, stroke', strokeT, strokeT'
+
+       , StrokeOpts(..)
+
+         -- * Inside/outside testing
+
+       , isInsideWinding, isInsideEvenOdd
 
          -- * Clipping
 
@@ -38,9 +44,9 @@ import Data.VectorSpace
 import Data.AffineSpace
 
 import Data.Semigroup hiding ((<>))
-import Data.Monoid
 import Control.Applicative (liftA2)
 import qualified Data.Foldable as F
+import Data.Default
 
 import Data.Typeable
 
@@ -51,21 +57,58 @@ import Data.Typeable
 -- | Convert a path into a diagram.  The resulting diagram has the
 --   names 0, 1, ... assigned to each of the path's vertices.
 --
+--   See also 'stroke'', which takes an extra options record allowing
+--   its behavior to be customized.
+--
 --   Note that a bug in GHC 7.0.1 causes a context stack overflow when
 --   inferring the type of @stroke@.  The solution is to give a type
 --   signature to expressions involving @stroke@, or (recommended)
 --   upgrade GHC (the bug is fixed in 7.0.2 onwards).
 stroke :: (Renderable (Path R2) b)
        => Path R2 -> Diagram b R2
-stroke p = mkAD (Prim p)
-                (getBounds p)
-                mempty
-                {-  XXX what to do here?
-                    fromNames $ zip ([0..] :: [Int])
-                                    (pathVertices p)  -- XXX names for Bezier
-                                                      --   control points too?
-                -}
-                (Query $ Any . flip isInsideWinding p)
+stroke = stroke' (def :: StrokeOpts ())
+
+-- | A variant of 'stroke' that takes an extra record of options to
+--   customize its behavior.  In particular:
+--
+--     * Names can be assigned to the path's vertices
+--
+--   'StrokeOpts' is an instance of 'Default', so @stroke' 'with' {
+--   ... }@ syntax may be used.
+stroke' :: (Renderable (Path R2) b, Atomic a) => StrokeOpts a -> Path R2 -> Diagram b R2
+stroke' opts p
+  = mkAD (Prim p)
+         (getBounds p)
+         (fromNames . concat $
+           zipWith zip (vertexNames opts) (pathVertices p))
+         (Query $ Any . flip isInsideWinding p)
+
+-- | A record of options that control how a path is stroked.
+--   @StrokeOpts@ is an instance of 'Default', so a @StrokeOpts@
+--   records can be created using @'with' { ... }@ notation.
+data StrokeOpts a
+  = StrokeOpts
+    { vertexNames :: [[a]]  -- ^ Atomic names that should be assigned
+                            --   to the vertices of the path so that
+                            --   they can be referenced later.  If
+                            --   there are not enough names, the extra
+                            --   vertices are not assigned names; if
+                            --   there are too many, the extra names
+                            --   are ignored.  Note that this is a
+                            --   /list of lists/ of names, since paths
+                            --   can consist of multiple trails.  The
+                            --   first list of names are assigned to
+                            --   the vertices of the first trail, the
+                            --   second list to the second trail, and
+                            --   so on.
+                            --
+                            --   The default value is the empty list.
+    }
+
+instance Default (StrokeOpts a) where
+  def = StrokeOpts
+        { vertexNames = []
+        }
 
 -- | A composition of 'stroke' and 'pathFromTrail' for conveniently
 --   converting a trail directly into a diagram.
@@ -79,6 +122,12 @@ strokeT :: (Renderable (Path R2) b)
         => Trail R2 -> Diagram b R2
 strokeT = stroke . pathFromTrail
 
+-- | A composition of 'stroke'' and 'pathFromTrail' for conveniently
+--   converting a trail directly into a diagram.
+strokeT' :: (Renderable (Path R2) b, Atomic a)
+         => StrokeOpts a -> Trail R2 -> Diagram b R2
+strokeT' opts = stroke' opts . pathFromTrail
+
 ------------------------------------------------------------
 --  Inside/outside testing
 ------------------------------------------------------------
@@ -86,9 +135,20 @@ strokeT = stroke . pathFromTrail
 cross :: R2 -> R2 -> Double
 cross (x,y) (x',y') = x * y' - y * x'
 
+-- XXX link to more info on this
+
+-- | Test whether the given point is inside the given (closed) path,
+--   by testing whether the point's /winding number/ is nonzero. Note
+--   that @False@ is /always/ returned for /open/ paths, regardless of
+--   the winding number.
 isInsideWinding :: P2 -> Path R2 -> Bool
 isInsideWinding p = (/= 0) . crossings p
 
+-- | Test whether the given point is inside the given (closed) path,
+--   by testing whether a ray extending from the point in the positive
+--   x direction crosses the path an even (outside) or odd (inside)
+--   number of times.  Note that @False@ is /always/ returned for
+--   /open/ paths, regardless of the number of crossings.
 isInsideEvenOdd :: P2 -> Path R2 -> Bool
 isInsideEvenOdd p = odd . crossings p
 
@@ -180,3 +240,8 @@ instance Transformable Clip where
 --   * The bounding function of the diagram is unaffected.
 clipBy :: (HasStyle a, V a ~ R2) => Path R2 -> a -> a
 clipBy = applyTAttr . Clip . (:[])
+
+-- XXX Should include a 'clipTo' function which clips a diagram AND
+-- restricts its bounding function.  It will have to take a *pointwise
+-- minimum* of the diagram's current bounding function and the path's
+-- bounding function.  Not sure of the best way to do this at the moment.
