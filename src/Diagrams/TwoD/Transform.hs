6fe525152aa322f4b9bdbb70770f79331cd3471e
Brent Yorgey <byorgey@cis.upenn.edu>
2011-07-30 17:29:11 +0000

    documentation typesetting fixes

--- src/Diagrams/TwoD/Transform.hs
+++ src/Diagrams/TwoD/Transform.hs
@@ -64,11 +64,11 @@ rotation ang = fromLinear r (linv r)
 -- | Rotate by the given angle. Positive angles correspond to
 --   counterclockwise rotation, negative to clockwise. The angle can
 --   be expressed using any type which is an instance of 'Angle'.  For
---   example, @rotate (1/4 :: 'CircleFrac')@, @rotate (pi/2 :: 'Rad')@, and
+--   example, @rotate (1\/4 :: 'CircleFrac')@, @rotate (pi\/2 :: 'Rad')@, and
 --   @rotate (90 :: 'Deg')@ all represent the same transformation, namely,
 --   a counterclockwise rotation by a right angle.
 --
---   Note that writing @rotate (1/4)@, with no type annotation, will
+--   Note that writing @rotate (1\/4)@, with no type annotation, will
 --   yield an error since GHC cannot figure out which sort of angle
 --   you want to use.  In this common situation you can use
 --   'rotateBy', which is specialized to take a 'CircleFrac' argument.
@@ -77,7 +77,7 @@ rotate = transform . rotation
 
 -- | A synonym for 'rotate', specialized to only work with
 --   @CircleFrac@ arguments; it can be more convenient to write
---   @rotateBy (1/4)@ than @'rotate' (1/4 :: 'CircleFrac')@.
+--   @rotateBy (1\/4)@ than @'rotate' (1\/4 :: 'CircleFrac')@.
 rotateBy :: (Transformable t, V t ~ R2) => CircleFrac -> t -> t
 rotateBy = transform . rotation
 
