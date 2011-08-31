9d75652ff1e53390421b989b9d3ccea3ec9cfe81
Brent Yorgey <byorgey@cis.upenn.edu>
2011-08-31 16:23:58 +0000

    start work on incorporating Polygons.hs

--- src/Diagrams/TwoD/Polygons.hs
+++ src/Diagrams/TwoD/Polygons.hs
@@ -16,7 +16,7 @@
 -----------------------------------------------------------------------------
 
 module Diagrams.TwoD.Polygons(
-        -- * Poligon's construction 
+        -- * Poligon's construction
         polyPolar, polySides, polyCyclic, polyRegular
         -- * Poligon's centers
         -- $centers
@@ -34,41 +34,52 @@ module Diagrams.TwoD.Polygons(
         -- * Rendering to 'Diagram's
         , polyPath, polyDiagram
         -- * Typed interface
-        , PolyData(..), StarGroup(..), Orientation(..), PolyOption(..), poDef, polyPoints
+        , PolyData(..), StarGroup(..), Orientation(..), PolygonOpts(..), poDef, polyPoints
     ) where
 
-import Data.Ord(comparing)
+import Data.Ord          (comparing)
 import Data.List
-import Control.Arrow ((&&&), (***))
-import Control.Monad(guard)
+import Control.Arrow     ((&&&), (***))
+import Control.Monad     (guard)
 
-import Data.VectorSpace  -- ((^/), sumV, magnitude, magnitudeSq, (<.>))
+import Data.AffineSpace  ((.-.), (.+^))
+import Data.VectorSpace
 import Data.Default
 
 import Diagrams.TwoD.Types
 import Diagrams.TwoD.Transform
 import Graphics.Rendering.Diagrams
-import Diagrams.Path(pathFromVertices, stroke)
+import Diagrams.Path
 
-data PolyData = PolyPolar [Angle] [Double] |
-                PolySides [Angle] [Double] |
-                PolyCyclic [Angle] |
-                PolyRegular Int
+data PolyData a = PolyPolar  [a] [Double]
+                | PolySides  [a] [Double]
+                | PolyCyclic [a]
+                | PolyRegular Int
+
 data StarGroup = StarFun (Int->Int) |
                  StarSkip Int
-data Orientation = OrientX | OrientY | OrientNone                 
 
-data PolyOption = PolyOption {
-        poPolyData      :: PolyData
+-- | Determine how a polygon should be oriented.
+data PolygonOrientation = NoOrient  -- ^ No special orientation; one
+                                    --   vertex will be at (1,0).
+                                    --   This is the default.
+                        | OrientToX -- ^ Orient so the botommost edge
+                                    --   is parallel to the x-axis.
+                        | OrientToY -- ^ Orient so the leftmost edge
+                                    --   is parallel to the y-axis.
+  deriving (Eq, Ord, Show, Read)
+
+data PolygonOpts a = PolygonOpts {
+        poPolyData      :: PolyData a
       , poStarGroup     :: StarGroup
-      , poOrientation   :: Orientation
+      , poOrientation   :: PolygonOrientation
       , poCenter        :: P2
     }
 
-instance Default PolyOption where
-    def = PolyOption (PolyRegular 3) (StarSkip 1) OrientX origin
-    
-polyPoints :: PolyOption -> [[P2]]
+instance Default (PolygonOpts a) where
+    def = PolygonOpts (PolyRegular 5) (StarSkip 1) OrientToX origin
+
+polyPoints :: Angle a => PolygonOpts a -> [[P2]]
 polyPoints po = sg
     where
         ps = case poPolyData po of
@@ -76,33 +87,33 @@ polyPoints po = sg
             PolySides ans szs -> polySides' ans szs
             PolyCyclic ans -> polyCyclic ans
             PolyRegular n -> polyRegular n
-        ori = case poOrientation po of 
-            OrientX -> orientX ps
-            OrientY -> orientY ps
+        ori = case poOrientation po of
+            OrientToX -> orientX ps
+            OrientToY -> orientY ps
         tr = ori -- translate (poCenter .-. centroid ori) ori
         sg = case poStarGroup po of
             StarFun f -> star f tr
             StarSkip n -> starSkip n tr
 
--- | Polygon from some center given by central angles and edges length. 
+-- | Polygon from some center given by central angles and edges length.
 --   There are /(n-1)/ angle and /n/ lengths for /n/-polygon.
 --   Extra angles or lengths are ignored.
-polyPolar :: [Angle] -> [Double] -> [P2]
+polyPolar :: Angle a => [a] -> [Double] -> [P2]
 polyPolar _ [] = []
 polyPolar ans (l:ls) = snd $ unzip xs
     where
         xs = (l, translateX l origin) : zipWith3 (\a s (s',x) -> (s, scale (s/s') $ rotate a x)) ans ls xs
 
--- | Polygon given by side-lengths and vertex-angles. 
+-- | Polygon given by side-lengths and vertex-angles.
 --   There are /(n-2)/ angle and /(n-1)/ lengths for /n/-polygon.
 --   Extra angles or lengths are ignored.
-polySides' :: [Angle] -> [Double] -> [P2]
-polySides' ans ls = scanl (\p v -> p .+^ v) origin $ map (.-. origin) $ flip polyPolar ls $ map (pi-) ans 
+polySides' :: Angle a => [a] -> [Double] -> [P2]
+polySides' ans ls = scanl (\p v -> p .+^ v) origin $ map (.-. origin) $ flip polyPolar ls $ map (convertAngle (tau/2 :: Rad) -) ans
 
 -- | Polygon is centered around 'origin'.
-polySides :: [Angle] -> [Double] -> [P2]
+polySides :: Angle a => [a] -> [Double] -> [P2]
 polySides ans ls = let p0 = polySides' ans ls in translate (origin .-. centroid p0) p0
-   
+
 {-
 angle :: (InnerSpace v, s ~ Scalar v, Floating s) => v -> v -> s
 angle v1 v2
@@ -114,7 +125,7 @@ angle v1 v2
 
 -- | Polygon with vertexes on circle given by central edges.
 --   There are just /(n-1)/ angle for /n/-polygon.
-polyCyclic :: [Angle] -> [P2]
+polyCyclic :: Angle a => [a] -> [P2]
 polyCyclic ans = polyPolar ans $ repeat 1
 
 -- | Regular /n/-polygon
@@ -129,7 +140,7 @@ groups :: (Int->Int) -> Int -> [[Int]]
 groups f n = unfoldr (\xs -> guard (not $ null xs) >> return (let zs = g (head xs) in (zs, xs \\ zs))) [0..n-1]
     where
         g x0 = snd $ last $ takeWhile (\(_,ys) -> ys == nub ys) $ iterate (\(k,ys) -> (f k `mod` n, k:ys)) (x0, [])
-    
+
 -- | Generate star from the list of points by function in Zn-ring.
 star :: (Int->Int) -> [P2] -> [[P2]]
 star f xs = map (map (xs!!)) $ groups f (length xs)
@@ -147,28 +158,16 @@ orient isMax fc ft xs = rotate a xs
         x' = mm (comparing (\(P z)->fc z)) [x1, x2]
         v = x' .-. x
         ex = ft 1 origin .-. origin
-        a = acos ((v <.> ex) / magnitude v)
+        a = Rad $ acos ((v <.> ex) / magnitude v)
 
 -- | takes lowermost vertex then take its lowermost neighbor and make this side horisontally
 orientX :: [P2] -> [P2]
 orientX = orient True snd translateX
-        
+
 -- | takes leftmost vertex then take its leftmost neighbor and make this side vertically
 orientY :: [P2] -> [P2]
 orientY = orient False fst translateY
-    
--- | Rounded polygons
--- | Regular poligons
--- | Well-known stars
-star5 = starSkip 5
-star6 = starSkip 6
-starDavid = star5
-starSolomon = starDavid
 
--- | Rendering to 'Diagram's
-polyPathes = map pathFromVertices . polyPoints
-polyDiagram = stroke . polyPath
-        
--- $centers        
+-- $centers
 --  * Centroid is calculated by <http://en.wikipedia.org/wiki/Centroid#Of_a_finite_set_of_points>
 --
