3a14e715b99506af238f056e11666ff04f929807
Brent Yorgey <byorgey@cis.upenn.edu>
2011-12-24 05:51:50 +0000

    a bit more documentation for animation-related stuff

--- src/Diagrams/Animation.hs
+++ src/Diagrams/Animation.hs
@@ -7,7 +7,10 @@
 -- License     :  BSD-style (see LICENSE)
 -- Maintainer  :  diagrams-discuss@googlegroups.com
 --
--- Tools for creating animated diagrams.
+-- Animated diagrams.  Most of the tools for working with animations
+-- can actually be found in "Data.Active".
+--
+-- XXX more documentation here
 --
 -----------------------------------------------------------------------------
 
@@ -15,10 +18,11 @@ module Diagrams.Animation where
 
 import Graphics.Rendering.Diagrams
 
-import Diagrams.Animation.Active
+import Diagrams.Animation.Active ()
 
 import Data.Active
 import Data.Semigroup
 
+-- | A value of type @QAnimation b v m@ is an animation that can be rendered by backspace @b@, with
 type QAnimation b v m = Active (QDiagram b v m)
 type Animation b v = QAnimation b v Any
