46009e565961ae88086b8324eec51fd72438a8d4
Brent Yorgey <byorgey@cis.upenn.edu>
2011-12-24 05:51:50 +0000

    a bit more documentation for animation-related stuff

--- src/Diagrams/Animation/Active.hs
+++ src/Diagrams/Animation/Active.hs
@@ -1,5 +1,17 @@
 {-# LANGUAGE TypeFamilies
   #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Diagrams.Animation.Active
+-- Copyright   :  (c) 2011 Brent Yorgey
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  byorgey@cis.upenn.edu
+--
+-- A few utilities and class instances for 'Active' (from the @active@
+-- package).
+-----------------------------------------------------------------------------
 
 module Diagrams.Animation.Active where
 
@@ -12,18 +24,25 @@ import Data.Active
 
 type instance V (Active a) = V a
 
--- XXX all these instances should go with their classes
+-- Yes, these are all orphan instances. Get over it.  We don't want to
+-- put them in the 'active' package because 'active' is supposed to be
+-- generally useful and shouldn't depend on diagrams.  We'd also
+-- rather not put them in diagrams-core so that diagrams-core doesn't
+-- have to depend on active.
 
 instance HasOrigin a => HasOrigin (Active a) where
   moveOriginTo = fmap . moveOriginTo
 
 instance Transformable a => Transformable (Active a) where
   transform = fmap . transform
-  
+
 instance HasStyle a => HasStyle (Active a) where
   applyStyle = fmap . applyStyle
-  
+
 instance PathLike p => PathLike (Active p) where
   pathLike st cl segs = pure (pathLike st cl segs)
-  
--- XXX Juxtaposable
+
+{-
+instance Juxtaposable a => Juxtaposable (Active a) where
+  juxtapose v a1 a2 =
+-}
