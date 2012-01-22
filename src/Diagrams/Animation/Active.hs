a84c81252bb80c9a52882252a20adfcfa434e9bb
Brent Yorgey <byorgey@cis.upenn.edu>
2012-01-22 19:22:58 +0000

    Juxtaposable instance for Active

--- src/Diagrams/Animation/Active.hs
+++ src/Diagrams/Animation/Active.hs
@@ -10,13 +10,33 @@
 -- Maintainer  :  byorgey@cis.upenn.edu
 --
 -- A few utilities and class instances for 'Active' (from the @active@
--- package).
+-- package).  In particular, this module defines
+--
+--   * An instance of 'V' for 'Active': @'V' ('Active' a) = 'V' a@
+--
+--   * 'HasOrigin', 'Transformable', and 'HasStyle' instances for
+--     'Active' which all work pointwise.
+--
+--   * A 'PathLike' instance for @'Active' p@ where @p@ is also
+--     'PathLike', which simply lifts a pathlike thing to a constant
+--     active value.
+--
+--   * A 'Juxtaposable' instance for @'Active' a@ where @a@ is also
+--     'Juxtaposable'.  An active value can be juxtaposed against
+--     another by doing the juxtaposition pointwise over time.  The
+--     era of @juxtapose v a1 a2@ will be the same as the era of @a2@,
+--     unless @a2@ is constant, in which case it will be the era of
+--     @a1@.  (Note that @juxtapose v a1 a2@ and @liftA2 (juxtapose v)
+--     a1 a2@ therefore have different semantics: the second is an
+--     active value whose era is the /combination/ of the eras of @a1@
+--     and @a2@).
+
 -----------------------------------------------------------------------------
 
 module Diagrams.Animation.Active where
 
 import Graphics.Rendering.Diagrams
-import Control.Applicative (pure)
+import Control.Applicative ((<$>), pure)
 
 import Diagrams.Path
 
@@ -42,7 +62,33 @@ instance HasStyle a => HasStyle (Active a) where
 instance PathLike p => PathLike (Active p) where
   pathLike st cl segs = pure (pathLike st cl segs)
 
-{-
+-- | An active value can be juxtaposed against another by doing the
+--   juxtaposition pointwise over time.  The era of @juxtapose v a1
+--   a2@ will be the same as the era of @a2@, unless @a2@ is constant,
+--   in which case it will be the era of @a1@.  (Note that @juxtapose
+--   v a1 a2@ and @liftA2 (juxtapose v) a1 a2@ therefore have
+--   different semantics: the second is an active value whose era is
+--   the /combination/ of the eras of @a1@ and @a2@).
 instance Juxtaposable a => Juxtaposable (Active a) where
+
   juxtapose v a1 a2 =
--}
+    onActive       -- a1
+      (\c1 ->        -- if a1 is constant, just juxtapose a2 pointwise with its value
+        juxtapose v c1 <$> a2
+      )
+                     -- if a1 is dynamic...
+      (onDynamic $ \s1 e1 d1 ->
+        onActive      -- a2
+          (\c2 ->      -- if a2 is constant, juxtapose pointwise with a1.  Since
+                       --   the result will no longer be constant, the result
+                       --   needs an era: we use a1's.
+            mkActive s1 e1 (\t -> juxtapose v (d1 t) c2)
+          )
+
+                       -- otherwise, juxtapose pointwise, without changing a2's era
+          (onDynamic $ \s2 e2 d2 ->
+            mkActive s2 e2 (\t -> juxtapose v (d1 t) (d2 t))
+          )
+          a2
+      )
+      a1
