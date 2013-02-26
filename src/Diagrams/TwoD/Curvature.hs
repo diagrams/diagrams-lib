6ee388b8b872ef03339a4f555443a10928558daf
Ryan Yates <ryates@cs.rochester.edu>
2013-02-25 20:26:14 -0500

    Working on Haddocks with diagrams!

--- src/Diagrams/TwoD/Curvature.hs
+++ src/Diagrams/TwoD/Curvature.hs
@@ -1,4 +1,4 @@
-﻿{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Diagrams.TwoD.Curvature
@@ -37,21 +37,71 @@ import Diagrams.TwoD.Vector
 -- Another way to measure this idea is to find the largest circle that we can
 -- push up against the curve and have it touch (locally) at exactly the point
 -- and not cross the curve.  This is a tangent circle.  The radius of that
--- circle is the "Radius of Curvature" and it is the reciprocal of curvature.
--- Note that if the circle is on the "left" of the curve, we have a positive
+-- circle is the \"Radius of Curvature\" and it is the reciprocal of curvature.
+-- Note that if the circle is on the \"left\" of the curve, we have a positive
 -- radius, and if it is to the right we have a negative radius.  Straight
 -- segments have an infinite radius which leads us to our representation.  We
 -- result in a pair of numerator and denominator so we can include infinity and
 -- zero for both the radius and the curvature.
-
+--
+--
+-- Lets consider the following curve:
+--
+-- <<diagramA.svg#diagram=diagramA&height=200&width=400>>
+--
+-- The curve starts with positive curvature,
+--
+-- <<diagramPos.svg#diagram=diagramPos&height=200&width=400>>
+--
+-- approaches zero curvature
+--
+-- <<diagramZero.svg#diagram=diagramZero&height=200&width=400>>
+--
+-- then has negative curvature
+--
+-- <<diagramNeg.svg#diagram=diagramNeg&height=200&width=400>>
+--
+-- > import Diagrams.TwoD.Curvature
+-- > 
+-- > segmentA = Cubic (12 & 0) (8 & 10) (20 & 8)
+-- > 
+-- > curveA = lw 0.1 . stroke . fromSegments $ [segmentA]
+-- > 
+-- > diagramA = pad 1.1 . centerXY $ curveA
+-- > 
+-- > diagramPos = diagramWithRadius 0.2
+-- > 
+-- > diagramZero = diagramWithRadius 0.5
+-- > 
+-- > diagramNeg = diagramWithRadius 0.8
+-- > 
+-- > diagramWithRadius t = pad 1.1 . centerXY 
+-- >          $ curveA 
+-- >         <> showCurvature segmentA t
+-- >          # withEnvelope (curveA :: D R2)
+-- >          # lw 0.05 # lc red
+-- > 
+-- > showCurvature bez@(Cubic b c d) t
+-- >   | p == 0 || v == 0 = mempty
+-- >   | otherwise        = (circle (abs r) # translate vpr
+-- >                     <> stroke (origin ~~ (origin .+^ vpr)))
+-- >                      # moveTo (origin .+^ atParam bez t)
+-- >   where
+-- >     vpr = r2 (normalized vp ^* r)
+-- >     v@(x,y) = unr2 $ firstDerivative b c d t
+-- >     vp = (-y) & x
+-- >     (p, q) = curvature bez t
+-- >     r = q / p
+-- >     firstDerivative b c d t = let tt = t*t in (3*(3*tt-4*t+1))*^b + (3*(2-3*t)*t)*^c + (3*tt)*^d
+--
 curvature :: Segment R2       -- ^ Segment to measure on.
           -> Double           -- ^ Parameter to measure at.
           -> (Double, Double) -- ^ Result is a numerator denominator pair (p,q) where
-                              -- the curvature is p/q and radius of curvature is q/p
+                              -- the curvature is p\/q and radius of curvature is q\/p
 curvature s = second sqrt . curvaturePair (fmap unr2 s) -- TODO: Use the generalized unr2
 
--- | With squaredCurvature we can compute values in spaces that do not support
--- sqrt and it is just as useful for relative ordering of curvatures or looking
+-- | With @squaredCurvature@ we can compute values in spaces that do not support
+-- 'sqrt' and it is just as useful for relative ordering of curvatures or looking
 -- for zeros.
 squaredCurvature :: Segment R2 -> Double -> (Double, Double)
 squaredCurvature s = first (join (*)) . curvaturePair (fmap unr2 s) -- TODO: Use the generalized unr2
