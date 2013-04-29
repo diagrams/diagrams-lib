c679eb2fb97cd207e1adc13975830bb57e806088
Brent Yorgey <byorgey@cis.upenn.edu>
2013-04-28 20:49:42 -0400

    set up package to copy diagrams-haddock images

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,29 @@
-#!/usr/bin/env runhaskell
-import Distribution.Simple
-main = defaultMain
+import           Data.List                 (isSuffixOf)
+import           Distribution.Simple
+import           Distribution.Simple.Setup (Flag (..), HaddockFlags,
+                                            haddockDistPref)
+import           Distribution.Simple.Utils (copyFiles)
+import           Distribution.Text         (display)
+import           Distribution.Verbosity    (normal)
+import           System.Directory          (getDirectoryContents)
+import           System.FilePath           ((</>))
+
+-- Ugly hack, logic copied from Distribution.Simple.Haddock
+haddockOutputDir :: Package pkg => HaddockFlags -> pkg -> FilePath
+haddockOutputDir flags pkg = destDir
+   where
+     baseDir = case haddockDistPref flags of
+                      NoFlag -> "."
+                      Flag x -> x
+     destDir = baseDir </> "doc" </> "html" </> display (packageName pkg)
+
+diagramsDir = "diagrams"
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+         { postHaddock = \args flags pkg lbi -> do
+             dias <- filter ("svg" `isSuffixOf`) `fmap` getDirectoryContents diagramsDir
+             copyFiles normal (haddockOutputDir flags pkg)
+               (map (\d -> ("", diagramsDir </> d)) dias)
+             postHaddock simpleUserHooks args flags pkg lbi
+         }
