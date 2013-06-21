3eebbb2c6c50229a1593d8ed2f101e4a622a1e0d
Brent Yorgey <byorgey@cis.upenn.edu>
2013-06-21 15:26:57 -0400

    cubicSpline does not actually need a Monoid constraint

--- src/Diagrams/CubicSpline.hs
+++ src/Diagrams/CubicSpline.hs
@@ -28,7 +28,7 @@ import           Diagrams.CubicSpline.Internal
 import           Diagrams.Located              (at)
 import           Diagrams.Path
 import           Diagrams.Segment
-import           Diagrams.Trail                (trailFromSegments)
+import           Diagrams.Trail                (emptyTrail, trailFromSegments)
 import           Diagrams.TrailLike            (TrailLike (..))
 
 -- for e.g. the Fractional (Double, Double) instance
@@ -41,8 +41,8 @@ import           Data.Semigroup
 --   vertices, with the first vertex as the starting point.  The first
 --   argument specifies whether the path should be closed.
 --   See: <http://mathworld.wolfram.com/CubicSpline.html>
-cubicSpline :: (Monoid t, TrailLike t, Fractional (V t)) => Bool -> [Point (V t)] -> t
-cubicSpline _      [] = mempty
+cubicSpline :: (TrailLike t, Fractional (V t)) => Bool -> [Point (V t)] -> t
+cubicSpline _      [] = trailLike (emptyTrail `at` origin)
 cubicSpline closed ps = flattenBeziers . map f . solveCubicSplineCoefficients closed . map unpack $ ps
   where
     f [a,b,c,d] = [a, (3*a+b)/3, (3*a+2*b+c)/3, a+b+c+d]
