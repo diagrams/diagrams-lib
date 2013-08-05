a664b946b1c44ef8fdb2ab0250f61b08b3a880f3
Brent Yorgey <byorgey@cis.upenn.edu>
2013-08-04 20:48:25 -0400

    D.CubicSpline: add example with diagrams-haddock

--- src/Diagrams/CubicSpline.hs
+++ src/Diagrams/CubicSpline.hs
@@ -12,8 +12,9 @@
 -- passing through a given sequence of points.  This module provides
 -- the 'cubicSpline' method, which can be used to create closed or
 -- open cubic splines from a list of points.  For access to the
--- internals of the spline generation algorithm, see
--- "Diagrams.CubicSpline.Internal".
+-- internals of the spline generation algorithm (including in
+-- particular a solver for cyclic tridiagonal systems of linear
+-- equations), see "Diagrams.CubicSpline.Internal".
 --
 -----------------------------------------------------------------------------
 module Diagrams.CubicSpline
@@ -42,7 +43,17 @@ import           Data.VectorSpace
 -- | Construct a spline path-like thing of cubic segments from a list of
 --   vertices, with the first vertex as the starting point.  The first
 --   argument specifies whether the path should be closed.
---   See: <http://mathworld.wolfram.com/CubicSpline.html>
+--
+--   <<diagrams/cubicSplineEx.svg#diagram=cubicSplineEx&width=600>>
+--
+--   > pts = map p2 [(0,0), (2,3), (5,-2), (-4,1), (0,3)]
+--   > dot = circle 0.2 # fc blue # lw 0
+--   > mkPath closed = position (zip pts (repeat dot))
+--   >              <> cubicSpline closed pts # lw 0.05
+--   > cubicSplineEx = (mkPath False ||| strutX 2 ||| mkPath True)
+--   >               # centerXY # pad 1.1
+--
+--   For more information, see <http://mathworld.wolfram.com/CubicSpline.html>.
 cubicSpline :: (TrailLike t, Fractional (V t)) => Bool -> [Point (V t)] -> t
 cubicSpline c [] = trailLike . closeIf c $ emptyLine `at` origin
 cubicSpline c ps = flattenBeziers . map f . solveCubicSplineCoefficients c . map unpack $ ps
