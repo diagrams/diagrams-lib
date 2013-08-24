33089cfd6a0ac95efe096f967f1b38ac36a8e5af
jeffrey rosenbluth <jeffrey.rosenbluth@gmail.com>
2013-08-23 23:46:59 -0400

    take 6 - this is getting embarassing

--- src/Diagrams/TwoD/Arrow.hs
+++ src/Diagrams/TwoD/Arrow.hs
@@ -257,7 +257,7 @@ connect' opts n1 n2 =
 connectTrail
   :: (Renderable (Path R2) b, IsName n1, IsName n2)
   => Trail R2 -> n1 -> n2 -> (Diagram b R2 -> Diagram b R2)
-connectTrail = connectTrail' def tr n1 n2
+connectTrail tr n1 n2 = connectTrail' def tr n1 n2
 
 connectTrail'
   :: (Renderable (Path R2) b, IsName n1, IsName n2)
