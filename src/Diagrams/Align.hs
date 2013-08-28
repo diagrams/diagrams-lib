4355bd348d741fdd416882fa5c843fe996a99c68
jeffrey rosenbluth <jeffrey.rosenbluth@gmail.com>
2013-08-28 17:14:47 -0400

    aligns compile

--- src/Diagrams/Align.hs
+++ src/Diagrams/Align.hs
@@ -21,7 +21,9 @@ module Diagrams.Align
        ( -- * Alignable class
 
          Alignable(..)
-       , alignByDefault
+       , alignBy'Default
+       , envelopeBoundary
+       , traceBoundary
 
          -- * General alignment functions
 
@@ -32,58 +34,92 @@ module Diagrams.Align
 
 import           Diagrams.Core
 
-import           Data.AffineSpace (alerp)
+import           Data.AffineSpace (alerp, (.-.))
 import           Data.VectorSpace
+import           Data.Maybe       (fromMaybe)
+import           Data.Ord         (comparing)
 
 import qualified Data.Map         as M
 import qualified Data.Set         as S
+import qualified Data.Foldable    as F
 
 -- | Class of things which can be aligned.
 class Alignable a where
 
   -- | @alignBy v d a@ moves the origin of @a@ along the vector
   --   @v@. If @d = 1@, the origin is moved to the edge of the
-  --   envelope in the direction of @v@; if @d = -1@, it moves to the
-  --   edge of the envelope in the direction of the negation of @v@.
+  --   boundary in the direction of @v@; if @d = -1@, it moves to the
+  --   edge of the boundary in the direction of the negation of @v@.
   --   Other values of @d@ interpolate linearly (so for example, @d =
   --   0@ centers the origin along the direction of @v@).
-  alignBy :: V a -> Scalar (V a) -> a -> a
+  alignBy' :: ( HasOrigin a, AdditiveGroup (V a), Num (Scalar (V a))
+              , Fractional (Scalar (V a)))
+            => (V a -> a -> Point (V a)) -> V a -> Scalar (V a) -> a -> a
+  alignBy' = alignBy'Default
+
+  defaultBoundary :: V a -> a -> Point (V a)
+
+  alignBy :: (HasOrigin a, Num (Scalar (V a)), Fractional (Scalar (V a)))
+           => V a -> Scalar (V a) -> a -> a
+  alignBy = alignBy' defaultBoundary
 
 -- | Default implementation of 'alignBy' for types with 'HasOrigin'
 --   and 'Enveloped' instances.
-alignByDefault :: (HasOrigin a, Enveloped a, Num (Scalar (V a)))
-               => V a -> Scalar (V a) -> a -> a
-alignByDefault v d a = moveOriginTo (alerp (envelopeP (negateV v) a)
-                                    (envelopeP v a)
-                                    ((d + 1) / 2))
-                             a
+alignBy'Default :: ( HasOrigin a, AdditiveGroup (V a), Num (Scalar (V a))
+                   , Fractional (Scalar (V a)))
+                 => (V a -> a -> Point (V a)) -> V a -> Scalar (V a) -> a -> a
+alignBy'Default boundary v d a = moveOriginTo (alerp (boundary (negateV v) a)
+                                    (boundary v a)
+                                    ((d + 1) / 2)) a
+
+envelopeBoundary :: Enveloped a => V a -> a -> Point (V a)
+envelopeBoundary = envelopeP
+
+traceBoundary :: Traced a => V a -> a -> Point (V a)
+traceBoundary v a = fromMaybe origin (maxTraceP origin v a)
+
+combineBoundaries
+  :: (F.Foldable f, InnerSpace (V a), Ord (Scalar (V a)))
+  => (V a -> a -> Point (V a)) -> (V a -> f a -> Point (V a))
+combineBoundaries b v fa
+    = b v $ F.maximumBy (comparing (magnitudeSq . (.-. origin) . b v)) fa
+--combineBoundaries b v = F.maximumBy (comparing (magnitudeSq . (.-. origin) . b v))
 
 instance (InnerSpace v, OrderedField (Scalar v)) => Alignable (Envelope v) where
-  alignBy = alignByDefault
+  defaultBoundary = envelopeBoundary
 
-instance (Enveloped b, HasOrigin b) => Alignable [b] where
-  alignBy = alignByDefault
+instance (InnerSpace v, OrderedField (Scalar v)) => Alignable (Trace v) where
+  defaultBoundary = traceBoundary
 
-instance (Enveloped b, HasOrigin b, Ord b) => Alignable (S.Set b) where
-  alignBy = alignByDefault
+instance (InnerSpace (V b), Ord (Scalar (V b)), Alignable b)
+       => Alignable [b] where
+  defaultBoundary = combineBoundaries defaultBoundary
 
-instance (Enveloped b, HasOrigin b) => Alignable (M.Map k b) where
-  alignBy = alignByDefault
+instance (InnerSpace (V b), Ord (Scalar (V b)), Alignable b)
+       => Alignable (S.Set b) where
+  defaultBoundary = combineBoundaries defaultBoundary
+
+instance (InnerSpace (V b), Ord (Scalar (V b)), Alignable b)
+       => Alignable (M.Map k b) where
+  defaultBoundary = combineBoundaries defaultBoundary
 
 instance ( HasLinearMap v, InnerSpace v, OrderedField (Scalar v)
          , Monoid' m
          ) => Alignable (QDiagram b v m) where
-  alignBy = alignByDefault
+  defaultBoundary = envelopeBoundary
 
--- | @align v@ aligns an enveloped object along the edge in the
+-- | @align v@ aligns an bounded object along the edge in the
 --   direction of @v@.  That is, it moves the local origin in the
---   direction of @v@ until it is on the edge of the envelope.  (Note
---   that if the local origin is outside the envelope to begin with,
+--   direction of @v@ until it is on the edge of the boundary.  (Note
+--   that if the local origin is outside the boundary to begin with,
 --   it may have to move \"backwards\".)
-align :: (Alignable a, Num (Scalar (V a))) => V a -> a -> a
+align :: ( Alignable a, HasOrigin a, Num (Scalar (V a))
+         , Fractional (Scalar (V a))) => V a -> a -> a
 align v = alignBy v 1
 
--- | @center v@ centers an enveloped object along the direction of
+-- | @center v@ centers an bounded object along the direction of
 --   @v@.
-center :: (Alignable a, Num (Scalar (V a))) => V a -> a -> a
+center :: ( Alignable a, HasOrigin a, Num (Scalar (V a))
+          , Fractional (Scalar (V a))) => V a -> a -> a
 center v = alignBy v 0
+
--- src/Diagrams/TwoD/Align.hs
+++ src/Diagrams/TwoD/Align.hs
@@ -45,22 +45,22 @@ import Data.VectorSpace
 -- | Align along the left edge, i.e. translate the diagram in a
 --   horizontal direction so that the local origin is on the left edge
 --   of the envelope.
-alignL :: (Alignable a, V a ~ R2) => a -> a
+alignL :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 alignL = align (negateV unitX)
 
 -- | Align along the right edge.
-alignR :: (Alignable a, V a ~ R2) => a -> a
+alignR :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 alignR = align unitX
 
 -- | Align along the top edge.
-alignT :: (Alignable a, V a ~ R2) => a -> a
+alignT :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 alignT = align unitY
 
 -- | Align along the bottom edge.
-alignB :: (Alignable a, V a ~ R2) => a -> a
+alignB :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 alignB = align (negateV unitY)
 
-alignTL, alignTR, alignBL, alignBR :: (Alignable a, V a ~ R2) => a -> a
+alignTL, alignTR, alignBL, alignBR :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 alignTL = alignT . alignL
 alignTR = alignT . alignR
 alignBL = alignB . alignL
@@ -75,23 +75,23 @@ alignBR = alignB . alignR
 --   * any other argument interpolates linearly between these.  For
 --     example, @alignX 0@ centers, @alignX 2@ moves the origin one
 --     \"radius\" to the right of the right edge, and so on.
-alignX :: (Alignable a, V a ~ R2) => Double -> a -> a
+alignX :: (Alignable a, HasOrigin a, V a ~ R2) => Double -> a -> a
 alignX = alignBy unitX
 
 -- | Like 'alignX', but moving the local origin vertically, with an
 --   argument of @1@ corresponding to the top edge and @(-1)@ corresponding
 --   to the bottom edge.
-alignY :: (Alignable a, V a ~ R2) => Double -> a -> a
+alignY :: (Alignable a, HasOrigin a, V a ~ R2) => Double -> a -> a
 alignY = alignBy unitY
 
 -- | Center the local origin along the X-axis.
-centerX  :: (Alignable a, V a ~ R2) => a -> a
+centerX  :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 centerX  = alignBy unitX 0
 
 -- | Center the local origin along the Y-axis.
-centerY  :: (Alignable a, V a ~ R2) => a -> a
+centerY  :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 centerY  = alignBy unitY 0
 
 -- | Center along both the X- and Y-axes.
-centerXY :: (Alignable a, V a ~ R2) => a -> a
+centerXY :: (Alignable a, HasOrigin a, V a ~ R2) => a -> a
 centerXY = centerX . centerY
