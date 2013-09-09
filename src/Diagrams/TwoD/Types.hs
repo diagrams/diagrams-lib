diagrams/diagrams-lib

commit 4853a0fc7e7e0aa819eda23e1a54f55688e35c07

clean up shadowing warnings

src/Diagrams/TwoD/Types.hs
@@ -80,16 +80,16 @@ instance Show R2 where
   showsPrec p (R2 (x,y)) = showParen (p >= 7) $
     showCoord x . showString " & " . showCoord y
    where
-    showCoord x | x < 0     = showParen True (shows x)
+    showCoord c | c < 0     = showParen True (shows c)
                 | otherwise = shows x
 
 instance Read R2 where
   readsPrec d r = readParen (d > app_prec)
-                  (\r -> [ (R2 (x,y), r''')
-                         | (x,r')    <- readsPrec (amp_prec + 1) r
-                         , ("&",r'') <- lex r'
-                         , (y,r''')  <- readsPrec (amp_prec + 1) r''
-                         ])
+                  (\rr -> [ (R2 (x,y), r''')
+                          | (x,r')    <- readsPrec (amp_prec + 1) rr
+                          , ("&",r'') <- lex r'
+                          , (y,r''')  <- readsPrec (amp_prec + 1) r''
+                          ])
                   r
     where
       app_prec = 10

commit 643f70ec7f8d71a6e72e841f3fa8d476661fea90

Fix Show instance of R2
4853a0fc7e7e0aa819eda23e1a54f55688e35c07 introduced a bug in which (R2 x y)
was shown as "x & x"

src/Diagrams/TwoD/Types.hs
@@ -102,7 +102,7 @@ instance Show R2 where
     showCoord x . showString " & " . showCoord y
    where
     showCoord c | c < 0     = showParen True (shows c)
-                | otherwise = shows x
+                | otherwise = shows c
 
 instance Read R2 where
   readsPrec d r = readParen (d > app_prec)