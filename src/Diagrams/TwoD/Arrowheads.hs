142c381010fabb40d8c1629156aeff6eb84dc611
Brent Yorgey <byorgey@cis.upenn.edu>
2013-09-23 15:20:58 -0400

    D.TwoD.Arrowheads: some documentation + warning cleanup

--- src/Diagrams/TwoD/Arrowheads.hs
+++ src/Diagrams/TwoD/Arrowheads.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts          #-}
-{-# LANGUAGE TypeFamilies              #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies              #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Diagrams.TwoD.Arrowheads
@@ -8,33 +8,32 @@
 -- License     :  BSD-style (see LICENSE)
 -- Maintainer  :  diagrams-discuss@googlegroups.com
 --
--- Standard arrowheads and tails. Normalized to fit inside a circle of
--- diameter 1 when line width is set to 0. Designed to be filled.
--- XXX Most of these heads could be used unfilled if a small enough
--- XXX stroke width is used.
+-- Standard arrowheads and tails. Each arrowhead or tail is designed
+-- to be drawn filled, with a line width of 0, and is normalized to
+-- fit inside a circle of diameter 1.
 --
 -----------------------------------------------------------------------------
 
 module Diagrams.TwoD.Arrowheads
-       ( ArrowHT
-       -- Heads --
-       , arrowheadTriangle
-       , arrowheadDart
-       , arrowheadSpike
-       , arrowheadThorn
-       , arrowheadMissile
-
-       , tri
+       (
+       -- * Arrowheads
+       -- ** Standard arrowheads
+         tri
        , dart
        , spike
        , thorn
        , missile
        , noHead
 
-       -- Tails --
-       , arrowtailQuill
-       , arrowtailBlock
+       -- ** Configurable arrowheads
+       , arrowheadTriangle
+       , arrowheadDart
+       , arrowheadSpike
+       , arrowheadThorn
+       , arrowheadMissile
 
+       -- * Arrow tails
+       -- ** Standard arrow tails
        , tri'
        , dart'
        , spike'
@@ -43,31 +42,36 @@ module Diagrams.TwoD.Arrowheads
        , noTail
        , quill
        , block
+
+       -- ** Configurable arrow tails
+
+       , arrowtailQuill
+       , arrowtailBlock
+
+       -- * Internals
+       , ArrowHT
        ) where
 
-import           Data.Default.Class
-import           Data.Maybe               (fromMaybe)
-import           Data.Functor             ((<$>))
-import           Data.Monoid              (mempty, (<>))
-import           Data.VectorSpace
 import           Data.AffineSpace
-import           Diagrams.Core            hiding (radius)
+import           Data.Functor            ((<$>))
+import           Data.Maybe              (fromMaybe)
+import           Data.Monoid             (mempty, (<>))
+import           Data.VectorSpace
+import           Diagrams.Core           hiding (radius)
 
-import           Diagrams.Trail
-import           Diagrams.TrailLike
+import           Diagrams.CubicSpline    (cubicSpline)
 import           Diagrams.Path
-import           Diagrams.TwoD.Path
 import           Diagrams.Segment
-import           Diagrams.TwoD.Shapes
-import           Diagrams.TwoD.Polygons
-import           Diagrams.TwoD.Arc        (arc')
-import           Diagrams.TwoD.Transform
+import           Diagrams.Trail
 import           Diagrams.TwoD.Align
+import           Diagrams.TwoD.Arc       (arc')
+import           Diagrams.TwoD.Path      ()
+import           Diagrams.TwoD.Polygons
+import           Diagrams.TwoD.Shapes
+import           Diagrams.TwoD.Transform
 import           Diagrams.TwoD.Types
-import           Diagrams.TwoD.Vector     (unit_X, unitX, e, direction)
-import           Diagrams.Util            (with, ( # ))
-import           Diagrams.CubicSpline     (cubicSpline)
-import           Diagrams.Parametric      (atStart, atParam)
+import           Diagrams.TwoD.Vector    (e, unitX, unit_X)
+import           Diagrams.Util           (with, ( # ))
 
 -----------------------------------------------------------------------------
 
@@ -82,6 +86,7 @@ scaleR = scale radius
 unit_X2 :: R2
 unit_X2 = scaleR unit_X
 
+closedPath :: (Floating (Scalar v), Ord (Scalar v), InnerSpace v) => Trail v -> Path v
 closedPath = pathFromTrail . closeTrail
 
 -- | Heads ------------------------------------------------------------------
@@ -99,13 +104,13 @@ arrowheadTriangle theta = aHead
 arrowheadDart :: Angle a => a -> ArrowHT
 arrowheadDart theta = aHead
   where
-    aHead size shaftWidth = (dart # moveOriginTo (dartVertices !! 2), joint)
+    aHead size shaftWidth = (dartP # moveOriginTo (dartVertices !! 2), joint)
       where
         a = toTurn theta
         r = radius * size
-        dart = polygon with {polyType = PolyPolar [a, 1/2 - a, 1/2 - a]
+        dartP = polygon with {polyType = PolyPolar [a, 1/2 - a, 1/2 - a]
                [r, r, 0.1 * size, r] ,polyOrient = NoOrient}
-        dartVertices =  (concat . pathVertices) $ dart
+        dartVertices =  (concat . pathVertices) $ dartP
         m = magnitude (dartVertices !! 1 .-. dartVertices !! 3)
         s = 1 - shaftWidth / m
         v1 = (dartVertices !! 1 .-. dartVertices !! 2) # scale s
@@ -131,24 +136,24 @@ arrowheadSpike theta = aHead
         b =  Rad $ asin ((shaftWidth / 2) / (radius  * size))
         c' = arc' radius (-b ) b # scale size
         joint = (closedPath $ (c')) # centerY # alignR
-        xWidth p = a + b
+        xWidth p = pa + pb
           where
-            a = fromMaybe 0 (magnitude <$> traceV origin unitX p)
-            b = fromMaybe 0 (magnitude <$> traceV origin unit_X p)
+            pa = fromMaybe 0 (magnitude <$> traceV origin unitX p)
+            pb = fromMaybe 0 (magnitude <$> traceV origin unit_X p)
 
 -- | Curved sides, linear concave base. Illustrator CS5 #3
 arrowheadThorn :: Angle a => a -> Double -> ArrowHT
 arrowheadThorn theta r = aHead
   where
-    aHead size shaftWidth = (thorn  # moveOriginTo (thornVertices !! 2), joint)
+    aHead size shaftWidth = (thornP # moveOriginTo (thornVertices !! 2), joint)
       where
         a  = e theta # scaleR
         c1 = curvedSide theta
         l1 = straight $ (reflectY a) ^-^ (unit_X2 # scale r)
         l2 = straight $ unit_X2 # scale r ^-^ a
         c2 = c1 # rotate (-theta)
-        thorn = (closedPath $ trailFromSegments [c1, l1, l2, c2]) # scale size
-        thornVertices =  (concat . pathVertices) $ thorn
+        thornP = (closedPath $ trailFromSegments [c1, l1, l2, c2]) # scale size
+        thornVertices =  (concat . pathVertices) $ thornP
         m = magnitude (thornVertices !! 1 .-. thornVertices !! 3)
         s = 1 - shaftWidth / m
         v1 = (thornVertices !! 1 .-. thornVertices !! 2) # scale s
@@ -176,7 +181,7 @@ smoothArrowhead f = aHead
   where
     aHead size shaftWidth = (h, j)
       where
-        (h', j') = f size shaftWidth
+        (h', _) = f size shaftWidth
         h = smooth $ pathVertices h'
         -- XXX replace square joint with actual shape
         j = square shaftWidth # scaleX 0.25 alignR
@@ -219,9 +224,9 @@ headToTail hd = tl
         j = reflectX j'
 
 arrowtailBlock :: Angle a => a -> ArrowHT
-arrowtailBlock theta =aTail
+arrowtailBlock theta = aTail
   where
-   aTail size shaftWidth = (t, mempty)
+   aTail size _ = (t, mempty)
       where
         t = square 1 # scaleX x # scaleY y # scale size # alignR
         a'  = e theta # scaleR
