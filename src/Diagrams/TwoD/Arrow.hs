c7cfbfdc97220c24211d7d08349359959fd2b94d
jeffrey rosenbluth <jeffrey.rosenbluth@gmail.com>
2013-10-11 15:51:05 -0400

    Fixed #127 for lens branch

--- src/Diagrams/TwoD/Arrow.hs
+++ src/Diagrams/TwoD/Arrow.hs
@@ -99,6 +99,7 @@ import           Data.Colour                      hiding (atop)
 import           Diagrams.Attributes
 import           Diagrams.Parametric
 import           Diagrams.Path
+import           Diagrams.Solve                   (quadForm)
 import           Diagrams.Tangent                 (tangentAtStart, tangentAtEnd)
 import           Diagrams.Trail
 import           Diagrams.TwoD.Arrowheads
@@ -229,15 +230,43 @@ spine tr tw hw size = tS <> tr # scale size <> hS
     hS = if hw > 0 then hSpine else mempty
     tS = if tw > 0 then tSpine else mempty
 
--- | Calculate the amount required to scale a shaft trail so that an arrow with
---   head width hw and tail width tw has offset t.
+--  | Calculate the amount required to scale a shaft trail so that an arrow with
+--    head width hw and tail width tw has offset t.
 scaleFactor :: Trail R2 -> Double -> Double -> Double -> Double
-scaleFactor tr tw hw t = (t - startOffset - endOffset) / magnitude (trailOffset tr)
+scaleFactor tr tw hw t
+
+  -- Let tv be a vector representing the tail width, i.e. a vector
+  -- of length tw tangent to the trail's start; similarly for hv.
+  -- Let v be the vector offset of the trail.
+  --
+  -- Then we want to find k such that
+  --
+  --   || tv + k*v + hv || = t.
+  --
+  -- We can solve by squaring both sides and expanding the LHS as a
+  -- dot product, resulting in a quadratic in k.
+
+  = case quadForm
+             (magnitudeSq v)
+             (2* (v <.> (tv ^+^ hv)))
+             (magnitudeSq (tv ^+^ hv) - t*t)
+    of
+      []  -> 1   -- no scale works, just return 1
+      [s] -> s   -- single solution
+      ss  -> maximum ss
+        -- we will usually get both a positive and a negative solution;
+        -- return the maximum (i.e. positive) solution
   where
-    tSpine = normalized . tangentAtStart $ tr
-    hSpine = normalized . tangentAtEnd $ tr
-    startOffset  = hw *^ hSpine <.> normalized (trailOffset tr)
-    endOffset    = tw *^ tSpine <.> normalized (trailOffset tr)
+    tv = tw *^ (tangentAtStart tr # normalized)
+    hv = hw *^ (tangentAtEnd   tr # normalized)
+    v  = trailOffset tr
+
+-- | Default number of iterations to try in hParam and tParam before giving up.
+stdIterations :: Int
+stdIterations = 64
+
+stdScale :: Double
+stdScale = 100
 
 -- | @arrow len@ creates an arrow of length @len@ with default parameters.
 arrow :: Renderable (Path R2) b => Double -> Diagram b R2
