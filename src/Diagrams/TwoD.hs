966faf87f011954af106f0d8eba93566dc66de24
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-15 09:45:13 -0500

    stop exporting 'e' from Diagrams.TwoD (and hence from Prelude)
    It is cute, but stepping on one-letter variable names is quite
    annoying for users in practice.  'e' can still be explicitly imported
    from Diagrams.TwoD.Vector.

--- src/Diagrams/TwoD.hs
+++ src/Diagrams/TwoD.hs
@@ -65,7 +65,7 @@ module Diagrams.TwoD
        , P2, p2, unp2, mkP2
        , T2
        , unitX, unitY, unit_X, unit_Y
-       , direction, angleBetween, fromDirection, e
+       , direction, angleBetween, fromDirection
 
          -- * Angles
        , tau
