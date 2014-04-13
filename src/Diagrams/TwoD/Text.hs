a3b4d3c811ee95d5859ba8dd8cc9de19398c3de0
Brent Yorgey <byorgey@gmail.com>
2014-04-13 17:00:25 -0400

    change default FontSize from Output 1 to Local 1
    Output 1 is not a good default since in most cases you can barely even
    see text at that size.  Local 1 seems reasonable, and should be
    basically backwards-compatible.

--- src/Diagrams/TwoD/Text.hs
+++ src/Diagrams/TwoD/Text.hs
@@ -171,7 +171,7 @@ instance AttributeClass FontSize
 type instance V FontSize = R2
 
 instance Default FontSize where
-    def = FontSize (Last (Output 1))
+    def = FontSize (Last (Local 1))
 
 instance Transformable FontSize where
   transform t (FontSize (Last s)) =
