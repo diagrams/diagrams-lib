7be96f62585bee0b5ae24c90c7d70ad13ab6407d
Daniel Bergey <bergey@alum.mit.edu>
2014-05-27 12:44:09 +0000

    Add UndecidableInstances pragma to Direction

--- src/Diagrams/Direction.hs
+++ src/Diagrams/Direction.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE TypeFamilies               #-}
 -----------------------------------------------------------------------------
 -- |
