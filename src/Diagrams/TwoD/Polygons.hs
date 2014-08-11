6bcd3e8703db980f794a1271eb931a3ccc37a04f
Brent Yorgey <byorgey@gmail.com>
2014-08-11 10:05:00 -0400

    D.TwoD.Polygons: documentation fix

--- src/Diagrams/TwoD/Polygons.hs
+++ src/Diagrams/TwoD/Polygons.hs
@@ -123,10 +123,9 @@ data PolyType = PolyPolar [Angle] [Double]
 -- | Determine how a polygon should be oriented.
 data PolyOrientation = NoOrient     -- ^ No special orientation; the first
                                     --   vertex will be at (1,0).
-                                    --   This is the default.
                      | OrientH      -- ^ Orient /horizontally/, so the
                                     --   bottommost edge is parallel to
-                                    --   the x-axis.
+                                    --   the x-axis.  This is the default.
                      | OrientV      -- ^ Orient /vertically/, so the
                                     --   leftmost edge is parallel to the
                                     --   y-axis.
