bc2f67604a6080903e7b3e91d1c2fbf261478722
Christopher Chalmers <c.chalmers@me.com>
2014-10-19 03:19:27 +0100

    More sensible linear and affine map classes.

--- src/Diagrams/LinearMap.hs
+++ src/Diagrams/LinearMap.hs
@@ -1,18 +1,11 @@
-{-# LANGUAGE DeriveDataTypeable    #-}
 {-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ConstraintKinds       #-}
-{-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE UndecidableInstances  #-}
 module Diagrams.LinearMap where
 
-import           Control.Applicative
-import           Control.Lens        hiding (pre)
+import           Control.Lens        hiding (lmap)
 import           Data.FingerTree     as FT
-import           Data.Typeable
+-- import           Data.Typeable
 -- import           GHC.Float
 
 import           Diagrams.Core
@@ -25,184 +18,109 @@ import           Diagrams.Trail      hiding (offset)
 import           Linear.Affine
 import           Linear.Metric
 
-import GHC.Exts (Constraint)
 import Linear.Vector
 
--- -- | Traversal over all the vectors of an object.
--- class HasVectors a b where
---   vectors :: Traversal a b (Vn a) (Vn b)
---   -- it's temping to make this overloaded, i.e.
---     -- HasVectors a v u n n' where vectors :: Traversal (a v n) (a u n') (v n) (u n')
--- 
--- instance HasVectors (Offset c v n) (Offset c u n') where
---   vectors f (OffsetClosed v) = OffsetClosed <$> f v
---   vectors _ OffsetOpen       = pure OffsetOpen
---   {-# INLINE vectors #-}
--- 
--- instance HasVectors (Segment c v n) (Segment c u n') where
---   vectors f (Linear offset)      = Linear <$> vectors f offset
---   vectors f (Cubic v1 v2 offset) = Cubic <$> f v1 <*> f v2 <*> vectors f offset
---   {-# INLINE vectors #-}
--- 
--- instance (Metric v, OrderedField n, Metric u, OrderedField n')
---     => HasVectors (SegTree v n) (SegTree u n') where
---   vectors = _Wrapped . traverse' . vectors
---   {-# INLINE vectors #-}
--- 
--- instance (Metric v, OrderedField n, Metric u, OrderedField n')
---     => HasVectors (Trail' l v n) (Trail' l u n') where
---   vectors f (Line st)        = Line <$> vectors f st
---   vectors f (Loop st offset) = Loop <$> vectors f st <*> vectors f offset
---   {-# INLINE vectors #-}
--- 
--- instance (Metric v, OrderedField n, Metric u, OrderedField n')
---     => HasVectors (Trail v n) (Trail u n') where
---   vectors f (Trail (Line st))        = Trail . Line <$> vectors f st
---   vectors f (Trail (Loop st offset)) = fmap Trail . Loop <$> vectors f st <*> vectors f offset
---   {-# INLINE vectors #-}
--- 
--- instance HasVectors (Point v n) (Point u n') where
---   vectors f (P v) = P <$> f v
---   {-# INLINE vectors #-}
--- 
--- instance HasVectors (FixedSegment v n) (FixedSegment u n') where
---   vectors f (FLinear p0 p1)      = FLinear <$> vectors f p0 <*> vectors f p1
---   vectors f (FCubic p0 p1 p2 p3) = FCubic <$> vectors f p0 <*> vectors f p1
---                                           <*> vectors f p2 <*> vectors f p3
---   {-# INLINE vectors #-}
--- 
--- instance (HasVectors a b) => HasVectors (Located a) (Located b) where
---   vectors f (Loc p a) = Loc <$> vectors f p <*> vectors f a
---   {-# INLINE vectors #-}
--- 
--- instance (Metric v, OrderedField n, Metric u, OrderedField n')
---     => HasVectors (Path v n) (Path u n') where
---   vectors = _Wrapped . traversed . vectors
---   {-# INLINE vectors #-}
--- 
--- floats2Doubles :: (V a ~ v, V a ~ V b, N a ~ Float, N b ~ Double, Functor v, HasVectors a b) => a -> b
--- floats2Doubles = over (vectors . mapped) float2Double
--- 
--- doubles2Floats :: (V a ~ v, V a ~ V b, N a ~ Double, N b ~ Float, Functor v, HasVectors a b) => a -> b
--- doubles2Floats = over (vectors . mapped) double2Float
--- 
+
 -- | Type for holding linear maps. Note that these are not affine transforms so 
 --   attemping apply a translation with 'LinearMap' will likely produce incorrect 
 --   results.
-newtype LinearMap v u n = LinearMap { linmap :: v n -> u n }
-  deriving Typeable
+newtype LinearMap v u n = LinearMap { lapply :: v n -> u n }
 
 toLinearMap :: Transformation v n -> LinearMap v v n
 toLinearMap (Transformation (m :-: _) _ _) = LinearMap m
 
--- vmap :: (InSpace v n a, InSpace u n b, HasVectors a b) => LinearMap v u n -> a -> b
--- vmap (LinearMap m) = over vectors m
+-- vmap :: (InSpace v n a, InSpace u n b, LinearMappable a b) => LinearMap v u n -> a -> b
+-- vmap (LinearMap m) = over vmap m
 
--- | Traversal over all the vectors of an object.
-class HasVectors a where
-  type Restrict a (v :: * -> *) n :: Constraint
-  type Restrict a (v :: * -> *) n = ()
+-- | Traversal over all the vmap of an object.
+class LinearMappable a b where
+  vmap :: (Vn a -> Vn b) -> a -> b
+  -- this uses a function instead of LinearMap so we can also use this class to 
+  -- change number types
 
-  vectors :: (Restrict a v n, Restrict a u n')
-    => Traversal (a v n) (a u n') (v n) (u n')
+-- | Apply a linear map.
+lmap :: (LinearMappable a b, N a ~ N b) => LinearMap (V a) (V b) (N a) -> a -> b
+lmap = vmap . lapply
 
-instance HasVectors (Offset c) where
-  vectors f (OffsetClosed v) = OffsetClosed <$> f v
-  vectors _ OffsetOpen       = pure OffsetOpen
-  {-# INLINE vectors #-}
+instance LinearMappable (Offset c v n) (Offset c u m) where
+  vmap f (OffsetClosed v) = OffsetClosed (f v)
+  vmap _ OffsetOpen       = OffsetOpen
+  {-# INLINE vmap #-}
 
-instance HasVectors (Segment c) where
-  vectors f (Linear offset)      = Linear <$> vectors f offset
-  vectors f (Cubic v1 v2 offset) = Cubic <$> f v1 <*> f v2 <*> vectors f offset
-  {-# INLINE vectors #-}
+instance LinearMappable (Segment c v n) (Segment c u m) where
+  vmap f (Linear offset)      = Linear (vmap f offset)
+  vmap f (Cubic v1 v2 offset) = Cubic (f v1) (f v2) (vmap f offset)
+  {-# INLINE vmap #-}
 
-instance HasVectors SegTree where
-  type Restrict SegTree v n = (Metric v, OrderedField n)
+instance (Metric v, Metric u, OrderedField n, OrderedField m)
+    => LinearMappable (SegTree v n) (SegTree u m) where
+  vmap f = over _Wrapped (fmap' (vmap f))
+  {-# INLINE vmap #-}
 
-  vectors = _Wrapped . traverse' . vectors
-  {-# INLINE vectors #-}
+instance (Metric v, Metric u, OrderedField n, OrderedField m)
+    => LinearMappable (Trail' l v n) (Trail' l u m) where
+  vmap f (Line st)        = Line (vmap f st)
+  vmap f (Loop st offset) = Loop (vmap f st) (vmap f offset)
+  {-# INLINE vmap #-}
 
-instance HasVectors (Trail' l) where
-  type Restrict (Trail' l) v n = (Metric v, OrderedField n)
+instance (Metric v, Metric u, OrderedField n, OrderedField m)
+    => LinearMappable (Trail v n) (Trail u m) where
+  vmap f (Trail (Line st))        = Trail $ Line (vmap f st)
+  vmap f (Trail (Loop st offset)) = Trail $ Loop (vmap f st) (vmap f offset)
+  {-# INLINE vmap #-}
 
-  vectors f (Line st)        = Line <$> vectors f st
-  vectors f (Loop st offset) = Loop <$> vectors f st <*> vectors f offset
-  {-# INLINE vectors #-}
+instance LinearMappable (Point v n) (Point u m) where
+  vmap f (P v) = P  (f v)
+  {-# INLINE vmap #-}
 
-instance HasVectors Trail where
-  type Restrict Trail v n = (Metric v, OrderedField n)
+instance LinearMappable (FixedSegment v n) (FixedSegment u m) where
+  vmap f (FLinear p0 p1)      = FLinear (vmap f p0) (vmap f p1)
+  vmap f (FCubic p0 p1 p2 p3) = FCubic (vmap f p0) (vmap f p1)
+                                       (vmap f p2) (vmap f p3)
+  {-# INLINE vmap #-}
 
-  vectors f (Trail (Line st))        = Trail . Line <$> vectors f st
-  vectors f (Trail (Loop st offset)) = fmap Trail . Loop <$> vectors f st <*> vectors f offset
-  {-# INLINE vectors #-}
+instance LinearMappable a b => LinearMappable (Located a) (Located b) where
+  vmap f (Loc p a) = Loc (vmap f p) (vmap f a)
+  {-# INLINE vmap #-}
 
-instance HasVectors Point where
-  vectors f (P v) = P <$> f v
-  {-# INLINE vectors #-}
-
-instance HasVectors FixedSegment where
-  vectors f (FLinear p0 p1)      = FLinear <$> vectors f p0 <*> vectors f p1
-  vectors f (FCubic p0 p1 p2 p3) = FCubic <$> vectors f p0 <*> vectors f p1
-                                          <*> vectors f p2 <*> vectors f p3
-  {-# INLINE vectors #-}
-
-
-instance HasVectors Path where
-  type Restrict Path v n = (Metric v, OrderedField n)
-  vectors = _Wrapped . traversed . locatedVectors
-    where
-      locatedVectors f (Loc p a) = Loc <$> vectors f p <*> vectors f a
-  {-# INLINE vectors #-}
+instance (Metric v, Metric u, OrderedField n, OrderedField m)
+    => LinearMappable (Path v n) (Path u m) where
+  vmap f = over (_Wrapped . mapped) (vmap f)
+  {-# INLINE vmap #-}
 
 -- | Affine linear maps. Unlike Transformation these do not have to be 
--- invertable so we can map between spaces.
-data AffineMap v u n = AffineMap
-  { _preTransform  :: Transformation v n
-  , _linearMap     :: LinearMap v u n
-  , _postTransform :: Transformation u n
-  } deriving Typeable
+--   invertable so we can map between spaces.
+data AffineMap v u n = AffineMap (LinearMap v u n) (u n)
 
-type Restricting v u n a = (Restrict a v n, Restrict a u n, AffineMappable a)
+toAffineMap :: Transformation v n -> AffineMap v v n
+toAffineMap (Transformation (m :-: _) _ v) = AffineMap (LinearMap m) v
 
-aapply :: (AffineMappable a, Restrict a v n, Restrict a u n, Additive u, Additive v, Num n)
-  => AffineMap v u n -> a v n -> a u n
-aapply (AffineMap pre (LinearMap l) post)
-  = amap (apply post . l . apply pre)
-         (papply post . over vectors l . papply pre)
+class (LinearMappable a b, N a ~ N b) => AffineMappable a b where
+  amap :: (Additive (V b), Num (N b)) => AffineMap (V a) (V b) (N b) -> a -> b
+  amap (AffineMap f _) = lmap f
+  {-# INLINE amap #-}
 
-class AffineMappable a where
-  amap :: (Restrict a v n, Restrict a u n') => (v n -> u n') -> (Point v n -> Point u n') -> a v n -> a u n'
+instance AffineMappable (Offset c v n) (Offset c u n)
+instance AffineMappable (Segment c v n) (Segment c u n)
+instance (Metric v, Metric u, OrderedField n) => AffineMappable (SegTree v n) (SegTree u n)
+instance (Metric v, Metric u, OrderedField n) => AffineMappable (Trail' l v n) (Trail' l u n)
+instance (Metric v, Metric u, OrderedField n) => AffineMappable (Trail v n) (Trail u n)
 
-  -- amap :: (LTarget a u n' ~ b, LinContr a, LinContr b, V a ~ v, N a ~ n, V b ~ u, N b ~ n')
-  --   => (v n -> u n') -> v n -> a -> b
+instance AffineMappable (Point v n) (Point u n) where
+  amap (AffineMap f v) (P p) = P (lapply f p ^+^ v)
+  {-# INLINE amap #-}
 
-instance AffineMappable (Offset c) where
-  amap f _ = over vectors f
+instance AffineMappable (FixedSegment v n) (FixedSegment u n) where
+  amap m (FLinear p0 p1)      = FLinear (amap m p0) (amap m p1)
+  amap m (FCubic p0 p1 p2 p3) = FCubic (amap m p0) (amap m p1) (amap m p2) (amap m p3)
+  {-# INLINE amap #-}
 
-instance AffineMappable (Segment c) where
-  amap f _ = over vectors f
+instance AffineMappable a b => AffineMappable (Located a) (Located b) where
+  amap m (Loc p x) = Loc (amap m p) (amap m x)
+  {-# INLINE amap #-}
 
-instance AffineMappable SegTree where
-  amap f _ = over vectors f
-
-instance AffineMappable (Trail' l) where
-  amap f _ = over vectors f
-
-instance AffineMappable Trail where
-  amap f _ = over vectors f
-
-instance AffineMappable Point where
-  amap _ f = f
-
-instance AffineMappable FixedSegment where
-  amap _ f (FLinear p0 p1)      = FLinear (f p0) (f p1)
-  amap _ f (FCubic p0 p1 p2 p3) = FCubic (f p0) (f p1) (f p2) (f p3)
-
--- instance AffineMappable a => AffineMappable (Located a) where
---   amap fv fp (Loc p x) = Loc (fp p) (amap fv fp x)
-
-instance AffineMappable Path where
-  amap a v (Path path) = Path (map (amapLoc a v) path)
-    where
-    amapLoc fv fp (Loc p x) = Loc (fp p) (amap fv fp x)
+instance (Metric v, Metric u, OrderedField n)
+    => AffineMappable (Path v n) (Path u n) where
+  amap m = over (_Wrapped . mapped) (amap m)
+  {-# INLINE amap #-}
   
