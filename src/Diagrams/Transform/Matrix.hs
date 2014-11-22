411093df89bfdd3c0c87166303bfddd9c5bee4f8
Christopher Chalmers <c.chalmers@me.com>
2014-11-22 16:54:38 +0000

    Changes to Matrix.

--- src/Diagrams/Transform/Matrix.hs
+++ src/Diagrams/Transform/Matrix.hs
@@ -9,31 +9,22 @@
 --
 -----------------------------------------------------------------------------
 
-module Diagrams.Trasform.Matrix where
+module Diagrams.Transform.Matrix where
 
 import           Control.Applicative
+import           Control.Arrow ((&&&))
 import           Control.Lens
 import           Data.Distributive
 import           Data.Foldable
 import           Data.Functor.Rep
 
-import           Diagrams.Core.Transform
+import           Diagrams.Core.Transform as D
+import           Diagrams.ThreeD.Types
+import           Diagrams.TwoD.Types
 
 import           Linear.Epsilon
 import           Linear.Matrix
-import           Linear.V2
-import           Linear.V3
 import           Linear.Vector
-
-mat22 :: (Epsilon n, Floating n) => Prism' (M22 n) (Transformation V2 n)
-mat22 = prism' mkMat fromMat
-  where
-    fromMat m = fromMatWithInv m <$> inv22 m
-
-mat33 :: (Epsilon n, Floating n) => Prism' (M33 n) (Transformation V3 n)
-mat33 = prism' mkMat fromMat
-  where
-    fromMat m = fromMatWithInv m <$> inv33 m
 
 -- | Build a matrix from a 'Transformation', ignoring the translation.
 mkMat :: (HasBasis v, Num n) => Transformation v n -> v (v n)
@@ -43,13 +34,27 @@ mkMat t = tabulate $ apply t . unit . el
 mkMatHomo :: Num n => Transformation V3 n -> M44 n
 mkMatHomo t = mkTransformationMat (mkMat t) (transl t)
 
+fromMat22 :: (Epsilon n, Floating n) => M22 n -> V2 n -> Maybe (T2 n)
+fromMat22 m v = flip (fromMatWithInv m) v <$> inv22 m
+
+fromMat33 :: (Epsilon n, Floating n) => M33 n -> V3 n -> Maybe (T3 n)
+fromMat33 m v = flip (fromMatWithInv m) v <$> inv33 m
+
 -- | Build a transform with a maxtrix along with its inverse.
 fromMatWithInv :: (Additive v, Distributive v, Foldable v, Num n)
   => v (v n) -- ^ matrix
   -> v (v n) -- ^ inverse
+  -> v n     -- ^ translation
   -> Transformation v n
-fromMatWithInv m m_ =
+fromMatWithInv m m_ v =
   Transformation ((*! m)            <-> (*! m_))
                  ((*! distribute m) <-> (*! distribute m_))
-                 zero
+                 v
+
+-- are these useful?
+mat22 :: (Epsilon n, Floating n) => Prism' (M22 n, V2 n) (T2 n)
+mat22 = prism' (mkMat &&& transl) (uncurry fromMat22)
+
+mat33 :: (Epsilon n, Floating n) => Prism' (M33 n, V3 n) (T3 n)
+mat33 = prism' (mkMat &&& transl) (uncurry fromMat33)
 
