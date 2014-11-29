e5fde7c53a325a57021465b40881b046b8e0bc62
Christopher Chalmers <c.chalmers@me.com>
2014-11-29 21:35:15 +0000

    Add Diagrams.Measured.

---
+++ src/Diagrams/Measured.hs
@@ -0,0 +1,38 @@
+module Diagrams.Measured where
+
+import Data.Semigroup
+import Data.Monoid.Coproduct
+import Data.Typeable
+import Data.Traversable (Traversable)
+
+import Diagrams.Core
+import Diagrams.Core.Measure
+import Diagrams.Core.Types
+import Diagrams.Util
+
+import Linear.Metric
+
+-- | Turn a measured diagram into a 'Diagram' with a 'DelayedLeaf'. The
+--   resulting diagram has no 'Envelope' or 'Trace and only 'local' units are
+--   scaled.
+measuredDiagram :: (Metric v, Traversable v, OrderedField n, Typeable n, Monoid' m)
+  => Measured n (QDiagram b v n m) -> QDiagram b v n m
+measuredDiagram md
+  = mkQD' (measuredLeaf md)
+          mempty -- envelope
+          mempty -- trace
+          mempty -- submap
+          mempty -- query
+
+-- | Turn a measured diagram into a 'DelayedLeaf'.
+measuredLeaf :: (Metric v, Traversable v, OrderedField n, Typeable n, Monoid' m)
+  => Measured n (QDiagram b v n m) -> QDiaLeaf b v n m
+measuredLeaf md = DelayedLeaf delayedPrim
+  where
+    delayedPrim da g n = unmeasure md (l,g,n)
+                           # transform tr
+                           # applyStyle sty
+      where
+        (tr, sty) = option mempty untangle . fst $ da
+        l         = avgScale tr
+
