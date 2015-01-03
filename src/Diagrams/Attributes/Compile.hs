589654aae8c2b2fecccd7227d46bf5bce33def33
Christopher Chalmers <c.chalmers@me.com>
2015-01-03 18:16:41 +0000

    GADTs for Attributes.Compile.

--- src/Diagrams/Attributes/Compile.hs
+++ src/Diagrams/Attributes/Compile.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies        #-}
 -----------------------------------------------------------------------------
