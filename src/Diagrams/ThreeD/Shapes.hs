061b0725f97de3c23df3736e2762aa7addc7d00c
Daniel Bergey <bergey@alum.mit.edu>
2015-05-28 15:40:03 +0000

    Add Skinned class, make 3D prims useful
    Make it easier to manipulate the 3D primitive solids Ellipsoid, Box,
    Frustum directly, without converting them to a Diagram.  This is useful
    for CSG.
    
    In particular, provide Traced, Enveloped instances, and define the
    Skinned instances in terms of these.
    
    The utility functions (sphere, cylinder, &c) now return the primitive
    types, not Diagram.  Add the Skinned class to convert these to Diagram,
    analogous to `stroke` in 2D.

--- src/Diagrams/ThreeD/Shapes.hs
+++ src/Diagrams/ThreeD/Shapes.hs
@@ -21,6 +21,7 @@ module Diagrams.ThreeD.Shapes
          Ellipsoid(..), sphere
        , Box(..), cube
        , Frustum(..) , frustum, cone, cylinder
+       , Skinned(..)
        ) where
 
 #if __GLASGOW_HASKELL__ < 710
@@ -53,21 +54,20 @@ instance Fractional n => Transformable (Ellipsoid n) where
 instance Fractional n => Renderable (Ellipsoid n) NullBackend where
   render _ _ = mempty
 
--- | A sphere of radius 1 with its center at the origin.
-sphere :: (Typeable n, OrderedField n, Renderable (Ellipsoid n) b) => QDiagram b V3 n Any
-sphere = mkQD (Prim $ Ellipsoid mempty)
-              (mkEnvelope sphereEnv)
-              (mkTrace sphereTrace)
-              mempty
-              (Query sphereQuery)
-  where
-    sphereEnv v         = 1 / norm v
-    sphereTrace (P p) v = mkSortedList $ quadForm a b c
-      where
+instance OrderedField n => Enveloped (Ellipsoid n) where
+    getEnvelope (Ellipsoid tr) = transform tr . mkEnvelope $ \v -> 1 / norm v
+
+instance OrderedField n => Traced (Ellipsoid n) where
+    getTrace (Ellipsoid tr) = transform tr . mkTrace $ \(P p) v -> let
         a  =      v `dot` v
         b  = 2 * (p `dot` v)
         c  =      p `dot` (p - 1)
-    sphereQuery v = Any $ quadrance (v .-. origin) <= 1
+        in
+         mkSortedList $ quadForm a b c
+
+-- | A sphere of radius 1 with its center at the origin.
+sphere :: Num n => Ellipsoid n
+sphere = Ellipsoid mempty
 
 data Box n = Box (Transformation V3 n)
   deriving Typeable
@@ -81,30 +81,31 @@ instance Fractional n => Transformable (Box n) where
 instance Fractional n => Renderable (Box n) NullBackend where
   render _ _ = mempty
 
+instance OrderedField n => Enveloped (Box n) where
+    getEnvelope (Box tr) = transform tr . mkEnvelope $ \v ->
+        maximum (map (v `dot`) corners) / quadrance v where
+          corners = mkR3 <$> [0,1] <*> [0,1] <*> [0,1]
+
+instance (Fractional n, Ord n) => Traced (Box n) where
+    getTrace (Box tr) = transform tr . mkTrace $ \p v -> let
+        (x0, y0, z0) = unp3 p
+        (vx, vy, vz) = unr3 v
+        intersections f d = case d of
+            0 -> []
+            _ -> [-f/d, (1-f)/d]
+        ts = concat $ zipWith intersections [x0,y0,z0] [vx,vy,vz]
+        atT t = p .+^ (t*^v)
+        range u = and [x >= 0, x <= 1, y >= 0, y <= 1, z >= 0, z <= 1] where
+          (x, y, z) = unp3 u
+        in
+         -- ts gives all intersections with the planes forming the box
+         -- filter keeps only those actually on the box surface
+         mkSortedList . filter (range . atT) $ ts where
+
 -- | A cube with side length 1, in the positive octant, with one
 -- vertex at the origin.
-cube :: (Typeable n, OrderedField n, Renderable (Box n) b) => QDiagram b V3 n Any
-cube = mkQD (Prim $ Box mempty)
-            (mkEnvelope boxEnv)
-            (mkTrace boxTrace)
-            mempty
-            (Query boxQuery)
-  where
-    corners = mkR3 <$> [0,1] <*> [0,1] <*> [0,1]
-    boxEnv v = maximum (map (v `dot`) corners) / quadrance v
-    -- ts gives all intersections with the planes forming the box
-    -- filter keeps only those actually on the box surface
-    boxTrace p v = mkSortedList . filter (range . atT) $ ts where
-      (x0, y0, z0) = unp3 p
-      (vx, vy, vz) = unr3 v
-      intersections f d = case d of
-          0 -> []
-          _ -> [-f/d, (1-f)/d]
-      ts = concat $ zipWith intersections [x0,y0,z0] [vx,vy,vz]
-      atT t = p .+^ (t*^v)
-    range u = and [x >= 0, x <= 1, y >= 0, y <= 1, z >= 0, z <= 1] where
-      (x, y, z) = unp3 u
-    boxQuery = Any . range
+cube :: Num n => Box n
+cube = Box mempty
 
 data Frustum n = Frustum n n (Transformation V3 n)
   deriving Typeable
@@ -118,35 +119,22 @@ instance Fractional n => Transformable (Frustum n) where
 instance Fractional n => Renderable (Frustum n) NullBackend where
   render _ _ = mempty
 
--- | A frustum of a right circular cone.  It has height 1 oriented
--- along the positive z axis, and radii r0 and r1 at Z=0 and Z=1.
--- 'cone' and 'cylinder' are special cases.
-frustum :: (TypeableFloat n, Renderable (Frustum n) b) => n -> n -> QDiagram b V3 n Any
-frustum r0 r1 = mkQD (Prim $ Frustum r0 r1 mempty)
-                 (mkEnvelope frEnv)
-                 (mkTrace frTrace)
-                 mempty
-                 (Query frQuery)
-  where
-    projectXY u = u ^-^ project unitZ u
-    frQuery p = Any $ x >= 0 && x <= 1 && a <= r where
-      (x, _, z) = unp3 p
-      r = r0 + (r1 - r0)*z
-      v = p .-. origin
-      a = norm $ projectXY v
+instance (OrderedField n, RealFloat n) => Enveloped (Frustum n) where
     -- The plane containing v and the z axis intersects the frustum in a trapezoid
     -- Test the four corners of this trapezoid; one must determine the Envelope
-    frEnv v = maximum . map (norm . project v . review r3CylindricalIso) $ corners
-      where
+    getEnvelope (Frustum r0 r1 tr) = transform tr . mkEnvelope $ \v ->let
         θ = v ^. _theta
         corners = [(r1,θ,1), (-r1,θ,1), (r0,θ,0), (-r0,θ,0)]
+        in
+         maximum . map (norm . project v . review r3CylindricalIso) $ corners
+
+instance (RealFloat n, Ord n) => Traced (Frustum n) where
     -- The trace can intersect the sides of the cone or one of the end
     -- caps The sides are described by a quadric equation; substitute
     -- in the parametric form of the ray but disregard any
     -- intersections outside z = [0,1] Similarly, find intersections
     -- with the planes z=0, z=1, but disregard any r>r0, r>r1
-    frTrace p v = mkSortedList $ filter zbounds (quadForm a b c) ++ ends
-      where
+    getTrace (Frustum r0 r1 tr) = transform tr . mkTrace $ \p v -> let
         (px, py, pz) = unp3 p
         (vx, vy, vz) = unr3 v
         ray t = p .+^ t *^ v
@@ -160,13 +148,58 @@ frustum r0 r1 = mkQD (Prim $ Frustum r0 r1 mempty)
         cap z = [ t | ray t ^. lensP . r3CylindricalIso . _1 < r0 + z * dr ]
           where
             t = (z - pz) / vz
+        in
+         mkSortedList $ filter zbounds (quadForm a b c) ++ ends
+
+-- | A frustum of a right circular cone.  It has height 1 oriented
+-- along the positive z axis, and radii r0 and r1 at Z=0 and Z=1.
+-- 'cone' and 'cylinder' are special cases.
+frustum :: Num n => n -> n -> Frustum n
+frustum r0 r1 = Frustum r0 r1 mempty
 
 -- | A cone with its base centered on the origin, with radius 1 at the
 -- base, height 1, and it's apex on the positive Z axis.
-cone :: (TypeableFloat n, Renderable (Frustum n) b) => QDiagram b V3 n Any
+cone :: Num n => Frustum n
 cone = frustum 1 0
 
 -- | A circular cylinder of radius 1 with one end cap centered on the
 -- origin, and extending to Z=1.
-cylinder :: (TypeableFloat n, Renderable (Frustum n) b) => QDiagram b V3 n Any
+cylinder :: Num n => Frustum n
 cylinder = frustum 1 1
+
+-- | Types which can answer a Query about points inside the geometric object.
+class Inside t where
+    inside :: t -> Query (V t) (N t) Any
+
+-- | Types which can be rendered as 3D Diagrams.
+class Skinned t where
+    skin :: (Renderable t b, N t ~ n, TypeableFloat n) => t  -> QDiagram b V3 n Any
+
+instance (Num n, Ord n) => Inside (Ellipsoid n) where
+    inside (Ellipsoid tr) = transform tr $
+                            Query $ \v -> Any $ quadrance (v .-. origin) <= 1
+
+instance OrderedField n => Skinned (Ellipsoid n) where
+    skin s = mkQD (Prim s) (getEnvelope s) (getTrace s) mempty (inside s)
+
+instance (Num n, Ord n) => Inside (Box n) where
+    inside (Box tr) = transform tr . Query $ Any . range where
+      range u = and [x >= 0, x <= 1, y >= 0, y <= 1, z >= 0, z <= 1] where
+          (x, y, z) = unp3 u
+
+instance OrderedField n => Skinned (Box n) where
+    skin s = mkQD (Prim s) (getEnvelope s) (getTrace s) mempty (inside s)
+
+instance (OrderedField n) => Inside (Frustum n) where
+    inside (Frustum r0 r1 tr)= transform tr $
+      Query $ \p -> let
+          (x, _, z) = unp3 p
+          r = r0 + (r1 - r0)*z
+          v = p .-. origin
+          a = norm $ projectXY v
+          projectXY u = u ^-^ project unitZ u
+          in
+           Any $ x >= 0 && x <= 1 && a <= r
+
+instance Skinned (Frustum n) where
+    skin s = mkQD (Prim s) (getEnvelope s) (getTrace s) mempty (inside s)
