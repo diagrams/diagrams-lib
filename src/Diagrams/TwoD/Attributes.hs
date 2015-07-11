510f403bed4efed4b47e5edd85c9c75adaa1fcde
Christopher Chalmers <c.chalmers@me.com>
2015-07-11 12:01:32 +0100

    Remove split fills

--- src/Diagrams/TwoD/Attributes.hs
+++ src/Diagrams/TwoD/Attributes.hs
@@ -58,9 +58,6 @@ module Diagrams.TwoD.Attributes (
   -- ** Fill color
   , fillColor, fc, fcA, recommendFillColor
 
-  -- * Compilation utilities
-  , splitTextureFills
-
   ) where
 
 import           Control.Lens                hiding (transform)
@@ -71,12 +68,7 @@ import           Data.Monoid.Recommend
 import           Data.Semigroup
 
 import           Diagrams.Attributes
-import           Diagrams.Attributes.Compile
 import           Diagrams.Core
-import           Diagrams.Core.Types         (RTree)
-import           Diagrams.Located            (unLoc)
-import           Diagrams.Path               (Path, pathTrails)
-import           Diagrams.Trail              (isLoop)
 import           Diagrams.TwoD.Types
 import           Diagrams.Util
 
@@ -402,29 +394,3 @@ fc = fillColor
 fcA :: (InSpace V2 n a, Floating n, Typeable n, HasStyle a) => AlphaColour Double -> a -> a
 fcA = fillColor
 
--- Split fills ---------------------------------------------------------
-
-data FillTextureLoops n = FillTextureLoops
-
-instance Typeable n => SplitAttribute (FillTextureLoops n) where
-  type AttrType (FillTextureLoops n) = FillTexture n
-  type PrimType (FillTextureLoops n) = Path V2 n
-
-  primOK _ = all (isLoop . unLoc) . pathTrails
-
--- | Push fill attributes down until they are at the root of subtrees
---   containing only loops. This makes life much easier for backends,
---   which typically have a semantics where fill attributes are
---   applied to lines/non-closed paths as well as loops/closed paths,
---   whereas in the semantics of diagrams, fill attributes only apply
---   to loops.
-splitTextureFills
-  :: forall b v n a. (
-#if __GLASGOW_HASKELL__ > 707
-                       Typeable v
-#else
-                       Typeable1 v
-#endif
-
-                     , Typeable n) => RTree b v n a -> RTree b v n a
-splitTextureFills = splitAttr (FillTextureLoops :: FillTextureLoops n)
