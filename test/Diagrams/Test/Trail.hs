729bd7b29f66788325be59587d778b507f90ece4
Mike Zuser <mikezuser@gmail.com>
2019-03-20 09:30:41 -0400

    Add tests for section on Trail' Line
    - Test that the endpoints of the section match the original
      parameters.
    - Test that the first and last segments of the section
      match sectioning the first and last segements.

--- test/Diagrams/Test/Trail.hs
+++ test/Diagrams/Test/Trail.hs
@@ -8,6 +8,9 @@ import           Instances
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
 
+import           Data.Fixed
+import           Data.List
+
 tests :: TestTree
 tests = testGroup "Trail"
   [ let wrap :: Trail' Line V2 Double -> Located (Trail V2 Double)
@@ -45,4 +48,48 @@ tests = testGroup "Trail"
     \t -> (reverseLocLoop . reverseLocLoop $ t) =~
           (t :: Located (Trail' Loop V2 Double))
 
+  , testProperty "section on Trail' Line endpoints match paramaters" $
+    \t (Param a) (Param b) ->
+      let t' = section (t :: Located (Trail' Line V2 Double)) a b
+      in  t `atParam` a =~ t' `atParam` 0 &&
+          t `atParam` b =~ t' `atParam` 1
+
+  , testProperty "section on Trail' Line matches section on FixedSegment" $
+    \t (Param a) (Param b) -> sectionTrailSectionFixedSegment t a b
+
   ]
+
+data Param = Param Double deriving Show
+
+instance Arbitrary Param where
+  arbitrary = Param <$> choose (-0.5, 1.5)
+
+sectionTrailSectionFixedSegment :: Located (Trail' Line V2 Double) -> Double -> Double -> Bool
+sectionTrailSectionFixedSegment t p1 p2
+  | null segs = t == t'
+  | otherwise = aSecT =~ aSecFS && bSecT =~ bSecFS
+  where
+    a = min p1 p2
+    b = max p1 p2
+    t' = section t a b
+
+    segs  = fixTrail $ mapLoc wrapLine t
+    segs' = fixTrail $ mapLoc wrapLine t'
+
+    aSecT = head segs'
+    bSecT = last segs'
+
+    (aSegIx, a') = splitParam a
+    (bSegIx, b') = splitParam b
+
+    aSecFS = section (segs !! floor aSegIx) a' x
+      where x = if aSegIx == bSegIx then b' else 1
+    bSecFS = section (segs !! floor bSegIx) x  b'
+      where x = if aSegIx == bSegIx then a' else 0
+
+    splitParam p | p <  0    = (0    , p           * n)
+                 | p >= 1    = (n - 1, 1 + (p - 1) * n)
+                 | otherwise = propFrac $  p       * n
+      where
+        propFrac x = let m = x `mod'` 1 in (x - m, m)
+        n = genericLength segs
